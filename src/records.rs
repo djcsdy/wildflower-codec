50cbfa9206cbfa2c4d743ad603c3431308402db2
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-02-19 02:40:45 +0000

    Add ColorTransformWithAlpha record.

--- src/records.rs
+++ src/records.rs
@@ -62,3 +62,15 @@ pub struct ColorTransform {
     pub green_addition_term: i16,
     pub blue_addition_term: i16,
 }
+
+/// A simple transformation of an RGBA color-with-alpha.
+pub struct ColorTransformationWithAlpha {
+    pub red_multiplication_term: Fixed8,
+    pub green_multiplication_term: Fixed8,
+    pub blue_multiplication_term: Fixed8,
+    pub alpha_multiplication_term: Fixed8,
+    pub red_addition_term: i16,
+    pub green_addition_term: i16,
+    pub blue_addition_term: i16,
+    pub alpha_addition_term: i16,
+}
