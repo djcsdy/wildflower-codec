229873c733e926015ad2ae47d8879a5599824017
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-02-19 01:15:02 +0000

    Add Fixed16 and Fixed8 record types.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod records;
 mod tags;
 
 fn main() {
---
+++ src/records.rs
@@ -0,0 +1,7 @@
+/// A fixed-point number consisting of a 16-bit whole part plus a 16-bit
+/// fractional part.
+struct Fixed16(i32);
+
+/// A fixed-point number consisting of a 8-bit whole part plus an 8-bit
+/// fractional part.
+struct Fixed8(i16);
