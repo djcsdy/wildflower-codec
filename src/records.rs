31a8667fedc3d371adbb4b697ed7d80d4845cf8c
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-02-19 02:05:17 +0000

    Add RGBA record.

--- src/records.rs
+++ src/records.rs
@@ -26,3 +26,11 @@ struct Rgb {
     pub green: u8,
     pub blue: u8,
 }
+
+/// An RGB color with an alpha component.
+struct Rgba {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+    pub alpha: u8,
+}
