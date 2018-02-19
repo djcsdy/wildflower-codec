8714d1ee817dc20aa64ff249e79c8cc24b03a051
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-02-19 02:46:06 +0000

    Add PlaceObjectTag.

--- src/tags.rs
+++ src/tags.rs
@@ -1,3 +1,5 @@
+use records::*;
+
 /// A Tag with an unknown or unsupported tag code.
 pub struct UnknownTag {
     pub tag_code: u16,
@@ -11,4 +13,24 @@ pub struct UnknownTag {
 pub struct InvalidTag {
     pub tag_code: u16,
     pub data: [u8],
+}
+
+/// Adds a character to the display list.
+pub struct PlaceObjectTag {
+    /// The ID of the character to place.
+    ///
+    /// The character must have previously be defined by another tag.
+    pub character_id: u16,
+
+    /// Depth of the character.
+    ///
+    /// Characters with higher depth values appear above characters with lower
+    /// depth values.
+    pub depth: u16,
+
+    /// Affine transformation matrix applied to the character.
+    pub matrix: Matrix,
+
+    /// Color transformation applied to the character.
+    pub color_transform: Option<ColorTransform>,
 }
