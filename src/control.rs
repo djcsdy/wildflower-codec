3d1bc374b61f53b5b8d0059e64354e08f3cb0831
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-02-20 07:34:43 +0000

    Add DefineScalingGridTag.

--- src/control.rs
+++ src/control.rs
@@ -98,3 +98,10 @@ pub struct SymbolClassRecord {
 pub struct MetadataTag {
     pub metadata: String
 }
+
+/// Defines a 9-slice grid that should be applied when scaling the specified
+/// character.
+pub struct DefineScalingGridTag {
+    pub character_id: u16,
+    pub splitter: Rectangle,
+}
