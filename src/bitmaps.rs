ff8563cb8aec5a44c559ea991cf73a19dba041d1
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-12-15 15:27:49 +0000

    Add DefineBitsJpeg2Tag and DefineBitsJpeg3Tag.

--- src/bitmaps.rs
+++ src/bitmaps.rs
@@ -1,8 +1,19 @@
 pub struct DefineBitsTag {
     pub character_id: u16,
-    pub jpeg_data: Vec<u8>
+    pub jpeg_data: Vec<u8>,
 }
 
 pub struct JpegTablesTag {
-    pub jpeg_data: Vec<u8>
+    pub jpeg_data: Vec<u8>,
+}
+
+pub struct DefineBitsJpeg2Tag {
+    pub character_id: u16,
+    pub image_data: Vec<u8>,
+}
+
+pub struct DefineBitsJpeg3Tag {
+    pub character_id: u16,
+    pub image_data: Vec<u8>,
+    pub bitmap_alpha_data: Vec<u8>,
 }
