fc6b179e6cb494b14029e40213cdc7e5dbf03db0
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-12-15 08:52:15 +0000

    Add DoActionTag.

--- src/actions.rs
+++ src/actions.rs
@@ -1,3 +1,39 @@
-pub struct ActionRecord {
-    // TODO
+pub struct DoActionTag {
+    pub actions: Vec<ActionRecord>
+}
+
+pub enum ActionRecord {
+    GoToFrame(GoToFrame),
+    GetUrl(GetUrl),
+    NextFrame,
+    PreviousFrame,
+    Play,
+    Stop,
+    ToggleQuality,
+    StopSounds,
+    WaitForFrame(WaitForFrame),
+    SetTarget(SetTarget),
+    GoToLabel(GoToLabel)
+}
+
+pub struct GoToFrame {
+    pub frame: u16
+}
+
+pub struct GetUrl {
+    pub url: String,
+    pub target: String
+}
+
+pub struct WaitForFrame {
+    pub frame: u16,
+    pub skip_count: u8
+}
+
+pub struct SetTarget {
+    pub target_name: String
+}
+
+pub struct GoToLabel {
+    pub label: String
 }
