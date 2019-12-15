bec4d2fce542de21459cd2d36bc3bcd949e210d6
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-12-15 16:11:05 +0000

    Add DefineBitsLosslessTag.

--- src/bitmaps.rs
+++ src/bitmaps.rs
@@ -1,3 +1,5 @@
+use super::common::Rgb;
+
 pub struct DefineBitsTag {
     pub character_id: u16,
     pub jpeg_data: Vec<u8>,
@@ -16,4 +18,22 @@ pub struct DefineBitsJpeg3Tag {
     pub character_id: u16,
     pub image_data: Vec<u8>,
     pub bitmap_alpha_data: Vec<u8>,
+}
+
+pub struct DefineBitsLosslessTag {
+    pub character_id: u16,
+    pub bitmap_width: u16,
+    pub bitmap_height: u16,
+    pub bitmap_data: BitmapData
+}
+
+pub enum BitmapData {
+    ColorMap8(ColorMapData),
+    Rgb15(Vec<Rgb>),
+    Rgb24(Vec<Rgb>)
+}
+
+pub struct ColorMapData {
+    pub color_table_rgb: Vec<Rgb>,
+    pub pixel_data: Vec<u8>
 }
