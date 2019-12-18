ac04332e9ec71b7b00822267ab246e8393bf842f
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-12-18 05:03:38 +0000

    Add DefineEditTextTag.

--- src/text.rs
+++ src/text.rs
@@ -27,3 +27,37 @@ pub struct GlyphEntry {
     pub glyph_index: u32,
     pub glyph_advance: i32,
 }
+
+pub struct DefineEditTextTag {
+    pub character_id: u16,
+    pub bounds: Rectangle,
+    pub word_wrap: bool,
+    pub multiline: bool,
+    pub password: bool,
+    pub read_only: bool,
+    pub auto_size: bool,
+    pub no_select: bool,
+    pub border: bool,
+    pub was_static: bool,
+    pub html: bool,
+    pub use_outlines: bool,
+    pub font_id: u16,
+    pub font_class: String,
+    pub font_height: u16,
+    pub text_color: Option<Rgba>,
+    pub max_length: Option<u16>,
+    pub align: Align,
+    pub left_margin: u16,
+    pub right_margin: u16,
+    pub indent: u16,
+    pub leading: i16,
+    pub variable_name: String,
+    pub initial_text: String
+}
+
+pub enum Align {
+    Left = 0,
+    Right = 1,
+    Center = 2,
+    Justify = 3,
+}
