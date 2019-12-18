ba804fbfb54ecf4d22f712f5bb1d27f7bc564c24
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-12-18 03:54:58 +0000

    Add DefineFontInfo2Tag.

--- src/text.rs
+++ src/text.rs
@@ -14,6 +14,24 @@ pub struct DefineFontInfoTag {
     pub code_table: CodeTable
 }
 
+pub struct DefineFontInfo2Tag {
+    pub font_id: u16,
+    pub font_name: String,
+    pub small_text: bool,
+    pub italic: bool,
+    pub bold: bool,
+    pub language_code: LanguageCode,
+    pub code_table: CodeTable
+}
+
+pub enum LanguageCode {
+    Latin = 1,
+    Japanese = 2,
+    Korean = 3,
+    SimplifiedChinese = 4,
+    TraditionalChinese = 5
+}
+
 pub enum CodeTable {
     Windows1252(Vec<u8>),
     JisX0201(Vec<u8>),
