039eca993a2c5d1da4b6498acbf1614557150f8d
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-12-18 04:18:28 +0000

    Add DefineFont2Tag.

--- src/text.rs
+++ src/text.rs
@@ -1,8 +1,25 @@
+use super::common::Rectangle;
 use super::shapes::Shape;
 
 pub struct DefineFontTag {
     pub font_id: u16,
-    pub glyph_shapes: Vec<Shape<(), ()>>
+    pub glyph_shapes: Vec<Shape<(), ()>>,
+}
+
+pub struct DefineFont2Tag {
+    pub font_id: u16,
+    pub small_text: bool,
+    pub italic: bool,
+    pub bold: bool,
+    pub language_code: LanguageCode,
+    pub font_name: String,
+    pub glyph_shapes: Vec<Shape<(), ()>>,
+    pub code_table: CodeTableWithKernings,
+    pub ascent: u16,
+    pub descent: u16,
+    pub leading: i16,
+    pub advances: Vec<i16>,
+    pub bounds: Vec<Rectangle>,
 }
 
 pub struct DefineFontInfoTag {
@@ -11,7 +28,7 @@ pub struct DefineFontInfoTag {
     pub small_text: bool,
     pub italic: bool,
     pub bold: bool,
-    pub code_table: CodeTable
+    pub code_table: CodeTable,
 }
 
 pub struct DefineFontInfo2Tag {
@@ -21,7 +38,7 @@ pub struct DefineFontInfo2Tag {
     pub italic: bool,
     pub bold: bool,
     pub language_code: LanguageCode,
-    pub code_table: CodeTable
+    pub code_table: CodeTable,
 }
 
 pub enum LanguageCode {
@@ -29,12 +46,37 @@ pub enum LanguageCode {
     Japanese = 2,
     Korean = 3,
     SimplifiedChinese = 4,
-    TraditionalChinese = 5
+    TraditionalChinese = 5,
 }
 
 pub enum CodeTable {
     Windows1252(Vec<u8>),
     JisX0201(Vec<u8>),
     ShiftJis(Vec<u16>),
-    Ucs2(Vec<u16>)
+    Ucs2(Vec<u16>),
+}
+
+pub enum CodeTableWithKernings {
+    Windows1252 {
+        character_codes: Vec<u8>,
+        kernings: Vec<Kerning<u8>>,
+    },
+    JisX0201 {
+        character_codes: Vec<u8>,
+        kernings: Vec<Kerning<u8>>,
+    },
+    ShiftJis {
+        character_codes: Vec<u16>,
+        kernings: Vec<Kerning<u16>>,
+    },
+    Ucs2 {
+        character_codes: Vec<u16>,
+        kernings: Vec<Kerning<u16>>,
+    },
+}
+
+pub struct Kerning<T> {
+    pub left_character_code: T,
+    pub right_character_code: T,
+    pub kerning_adjustment: i16,
 }
