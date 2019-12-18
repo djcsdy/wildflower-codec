3c2959f8a5e4cb1b9c5578ccf1b6a54f3c0e19c9
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-12-18 00:55:09 +0000

    Add DefineFontInfoTag.

--- src/text.rs
+++ src/text.rs
@@ -4,3 +4,19 @@ pub struct DefineFontTag {
     pub font_id: u16,
     pub glyph_shapes: Vec<Shape<(), ()>>
 }
+
+pub struct DefineFontInfoTag {
+    pub font_id: u16,
+    pub font_name: String,
+    pub small_text: bool,
+    pub italic: bool,
+    pub bold: bool,
+    pub code_table: CodeTable
+}
+
+pub enum CodeTable {
+    Windows1252(Vec<u8>),
+    JisX0201(Vec<u8>),
+    ShiftJis(Vec<u16>),
+    Ucs2(Vec<u16>)
+}
