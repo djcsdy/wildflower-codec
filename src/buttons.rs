f2491029d97a78c3644715d26f3b9d8e074cea0d
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-12-29 21:58:07 +0000

    Add DefineButton2Tag.

--- src/buttons.rs
+++ src/buttons.rs
@@ -8,6 +8,13 @@ pub struct DefineButtonTag {
     pub actions: Vec<ActionRecord>,
 }
 
+pub struct DefineButton2Tag {
+    pub button_id: u16,
+    pub track_as_menu: bool,
+    pub characters: Vec<ButtonRecord2>,
+    pub actions: Vec<ButtonConditionActionRecord>,
+}
+
 pub struct ButtonRecord {
     pub button_state_hit_test: bool,
     pub button_state_down: bool,
@@ -23,4 +30,18 @@ pub struct ButtonRecord2 {
     pub color_transform: ColorTransformWithAlpha,
     pub filter_list: Vec<Filter>,
     pub blend_mode: BlendMode,
+}
+
+pub struct ButtonConditionActionRecord {
+    pub condition_idle_to_over_down: bool,
+    pub condition_out_down_to_idle: bool,
+    pub condition_out_down_to_over_down: bool,
+    pub condition_over_down_to_out_down: bool,
+    pub condition_over_down_to_over_up: bool,
+    pub condition_over_up_to_over_down: bool,
+    pub condition_over_up_to_idle: bool,
+    pub condition_idle_to_over_up: bool,
+    pub condition_key_press: u8,
+    pub condition_over_down_to_idle: bool,
+    pub actions: Vec<ActionRecord>,
 }
