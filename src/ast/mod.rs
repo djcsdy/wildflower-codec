05d6ea2ccfa01da89d95905493604914e52b7520
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-12-30 02:55:51 +0000

    Move AST data types into new "ast" module.

---
+++ src/ast/mod.rs
@@ -0,0 +1,15 @@
+pub mod actions;
+pub mod bitmaps;
+pub mod buttons;
+pub mod common;
+pub mod control;
+pub mod display_list;
+pub mod fonts;
+pub mod metadata;
+pub mod shape_morphing;
+pub mod shapes;
+pub mod sounds;
+pub mod sprites;
+pub mod styles;
+pub mod text;
+pub mod video;
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,4 @@
 #[macro_use]
 extern crate bitflags;
 
-pub mod actions;
-pub mod bitmaps;
-pub mod buttons;
-pub mod common;
-pub mod control;
-pub mod display_list;
-pub mod fonts;
-pub mod metadata;
-pub mod shape_morphing;
-pub mod shapes;
-pub mod sounds;
-pub mod sprites;
-pub mod styles;
-pub mod text;
-pub mod video;
+pub mod ast;
