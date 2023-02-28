183c55223ec79df0ce444e4fee8db67e53d1701e
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-02-28 20:28:33 +0000

    Add SwfReader::new function.

--- src/decode/reader.rs
+++ src/decode/reader.rs
@@ -4,3 +4,9 @@ use std::io::Read;
 pub struct SwfReader<R: Read> {
     bit_reader: SwfBitReader<R>,
 }
+
+impl<R: Read> SwfReader<R> {
+    pub fn new<I: Into<SwfBitReader<R>>>(bit_reader: I) -> SwfReader<R> {
+        SwfReader { bit_reader }
+    }
+}
