382a7c950d6f670d873c97aee8e8e342199bc41b
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-02-28 20:46:03 +0000

    Add Header struct.

--- src/ast/header.rs
+++ src/ast/header.rs
@@ -1,5 +1,15 @@
+use ast::common::{Fixed8, Rectangle};
+
 pub enum Compression {
     None,
     Zlib,
     Lzma,
+}
+
+pub struct Header {
+    pub compression: Compression,
+    pub version: u8,
+    pub frame_size: Rectangle,
+    pub frame_rate: Fixed8,
+    pub frame_count: u16,
 }
