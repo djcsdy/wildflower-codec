bb428eb7031d9e99857d7eca0220972b956b3f8f
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-02-28 21:33:56 +0000

    Add SwfBitReader::read_i8_into function.

--- src/decode/bit_reader.rs
+++ src/decode/bit_reader.rs
@@ -36,6 +36,10 @@ impl<R: Read> SwfBitReader<R> {
         self.inner.read_i32::<LittleEndian>()
     }
 
+    pub fn read_i8_into(&mut self, buf: &mut [i8]) -> Result<()> {
+        self.inner.read_i8_into(buf)
+    }
+
     pub fn read_u8(&mut self) -> Result<u8> {
         self.inner.read_u8()
     }
