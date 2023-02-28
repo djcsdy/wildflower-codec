ef3a31cce0418211d37420cc9fc8815dfe4cd44e
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-02-28 19:51:20 +0000

    Add SwfFile::open function.

--- src/decode/file.rs
+++ src/decode/file.rs
@@ -1,5 +1,13 @@
 use std::fs::File;
+use std::io::Result;
+use std::path::Path;
 
 pub struct SwfFile {
     file: File,
+}
+
+impl SwfFile {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<SwfFile> {
+        File::open(path).map(|file| SwfFile { file })
+    }
 }
