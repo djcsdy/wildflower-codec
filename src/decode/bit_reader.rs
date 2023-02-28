2bb116480f48e12e75823c9fbf83c02462712b06
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-02-28 22:56:09 +0000

    SwfBitReader: Call align_byte() automatically before reading byte-aligned types.

--- src/decode/bit_reader.rs
+++ src/decode/bit_reader.rs
@@ -32,46 +32,57 @@ impl<R: Read> SwfBitReader<R> {
     }
 
     pub fn read_i8(&mut self) -> Result<i8> {
+        self.align_byte();
         self.inner.read_i8()
     }
 
     pub fn read_i16(&mut self) -> Result<i16> {
+        self.align_byte();
         self.inner.read_i16::<LittleEndian>()
     }
 
     pub fn read_i32(&mut self) -> Result<i32> {
+        self.align_byte();
         self.inner.read_i32::<LittleEndian>()
     }
 
     pub fn read_i8_into(&mut self, buf: &mut [i8]) -> Result<()> {
+        self.align_byte();
         self.inner.read_i8_into(buf)
     }
 
     pub fn read_i16_into(&mut self, buf: &mut [i16]) -> Result<()> {
+        self.align_byte();
         self.inner.read_i16_into::<LittleEndian>(buf)
     }
 
     pub fn read_u8(&mut self) -> Result<u8> {
+        self.align_byte();
         self.inner.read_u8()
     }
 
     pub fn read_u16(&mut self) -> Result<u16> {
+        self.align_byte();
         self.inner.read_u16::<LittleEndian>()
     }
 
     pub fn read_u32(&mut self) -> Result<u32> {
+        self.align_byte();
         self.inner.read_u32::<LittleEndian>()
     }
 
     pub fn read_u8_into(&mut self, buf: &mut [u8]) -> Result<()> {
+        self.align_byte();
         self.inner.read_exact(buf)
     }
 
     pub fn read_u16_into(&mut self, buf: &mut [u16]) -> Result<()> {
+        self.align_byte();
         self.inner.read_u16_into::<LittleEndian>(buf)
     }
 
     pub fn read_u24_into(&mut self, buf: &mut [u32]) -> Result<()> {
+        self.align_byte();
         for i in 0..buf.len() {
             let mut buf2 = [0u8; 3];
             self.inner.read_exact(&mut buf2)?;
@@ -81,36 +92,43 @@ impl<R: Read> SwfBitReader<R> {
     }
 
     pub fn read_u32_into(&mut self, buf: &mut [u32]) -> Result<()> {
+        self.align_byte();
         self.inner.read_u32_into::<LittleEndian>(buf)
     }
 
     pub fn read_u64_into(&mut self, buf: &mut [u64]) -> Result<()> {
+        self.align_byte();
         self.inner.read_u64_into::<LittleEndian>(buf)
     }
 
     pub fn read_fixed16(&mut self) -> Result<Fixed16> {
+        self.align_byte();
         let mut buf = [0u8; 4];
         self.inner.read_exact(&mut buf)?;
         Ok(Fixed16::from_bytes(&buf))
     }
 
     pub fn read_fixed8(&mut self) -> Result<Fixed8> {
+        self.align_byte();
         let mut buf = [0u8; 2];
         self.inner.read_exact(&mut buf)?;
         Ok(Fixed8::from_bytes(&buf))
     }
 
     pub fn read_float16(&mut self) -> Result<Float16> {
+        self.align_byte();
         let mut buf = [0u8; 2];
         self.inner.read_exact(&mut buf)?;
         Ok(Float16::from_bytes(&buf))
     }
 
     pub fn read_f32(&mut self) -> Result<f32> {
+        self.align_byte();
         self.inner.read_f32::<LittleEndian>()
     }
 
     pub fn read_f64(&mut self) -> Result<f64> {
+        self.align_byte();
         self.inner.read_f64::<LittleEndian>()
     }
 }
