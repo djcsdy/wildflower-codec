5552a8c92eec3be3a81a1fd8d183fe700444a10b
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-02 18:34:56 +0000

    Add read_wait_for_frame function.

--- src/decode/tag/actions.rs
+++ src/decode/tag/actions.rs
@@ -1,4 +1,4 @@
-use crate::ast::actions::{GetUrl, GoToFrame};
+use crate::ast::actions::{GetUrl, GoToFrame, WaitForFrame};
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::tag_body_reader::SwfTagBodyReader;
 use std::io::{Read, Result};
@@ -12,4 +12,10 @@ fn read_get_url<R: Read>(reader: &mut SwfTagBodyReader<R>) -> Result<GetUrl> {
     let url = reader.read_string()?;
     let target = reader.read_string()?;
     Ok(GetUrl { url, target })
+}
+
+fn read_wait_for_frame<R: Read>(reader: &mut SwfTagBodyReader<R>) -> Result<WaitForFrame> {
+    let frame = reader.read_u16()?;
+    let skip_count = reader.read_u8()?;
+    Ok(WaitForFrame { frame, skip_count })
 }
