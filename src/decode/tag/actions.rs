0463c2e2b3f7687e260703fb459f6af634bf3ad3
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-02 18:33:17 +0000

    Add read_get_url function.

--- src/decode/tag/actions.rs
+++ src/decode/tag/actions.rs
@@ -1,4 +1,4 @@
-use crate::ast::actions::GoToFrame;
+use crate::ast::actions::{GetUrl, GoToFrame};
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::tag_body_reader::SwfTagBodyReader;
 use std::io::{Read, Result};
@@ -6,4 +6,10 @@ use std::io::{Read, Result};
 fn read_go_to_frame<R: Read>(reader: &mut SwfTagBodyReader<R>) -> Result<GoToFrame> {
     let frame = reader.read_u16()?;
     Ok(GoToFrame { frame })
+}
+
+fn read_get_url<R: Read>(reader: &mut SwfTagBodyReader<R>) -> Result<GetUrl> {
+    let url = reader.read_string()?;
+    let target = reader.read_string()?;
+    Ok(GetUrl { url, target })
 }
