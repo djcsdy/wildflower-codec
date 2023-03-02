2304f915448c00da7e87610085500a009b45069f
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-02 15:50:19 +0000

    CountingReader: Add maximum length functionality.

--- src/decode/counting_reader.rs
+++ src/decode/counting_reader.rs
@@ -1,13 +1,31 @@
-use std::io::{IoSliceMut, Read, Result};
+use std::cmp::min;
+use std::io::{Read, Result};
 
 pub struct CountingReader<R: Read> {
     inner: R,
     count: usize,
+    max_length: Option<usize>,
 }
 
 impl<R: Read> CountingReader<R> {
     pub fn new(inner: R) -> CountingReader<R> {
-        CountingReader { inner, count: 0 }
+        CountingReader {
+            inner,
+            count: 0,
+            max_length: None,
+        }
+    }
+
+    pub fn take(inner: R, max_length: usize) -> CountingReader<R> {
+        CountingReader {
+            inner,
+            count: 0,
+            max_length: Some(max_length),
+        }
+    }
+
+    pub fn remaining(&self) -> Option<usize> {
+        self.max_length.map(|max_length| max_length - self.count)
     }
 
     pub fn into_inner(self) -> R {
@@ -17,13 +35,10 @@ impl<R: Read> CountingReader<R> {
 
 impl<R: Read> Read for CountingReader<R> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        let size = self.inner.read(buf)?;
-        self.count += size;
-        Ok(size)
-    }
-
-    fn read_vectored(&mut self, bufs: &mut [IoSliceMut<'_>]) -> Result<usize> {
-        let size = self.inner.read_vectored(bufs)?;
+        let len = self
+            .remaining()
+            .map_or(buf.len(), |remaining| min(remaining, buf.len()));
+        let size = self.inner.read(&mut buf[0..len])?;
         self.count += size;
         Ok(size)
     }
