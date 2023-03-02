8f85c2a78a5fa5946d8ff9f149c1d3dc211285c4
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-02 00:47:20 +0000

    Add SwfReader::read_header function.

--- src/decode/reader.rs
+++ src/decode/reader.rs
@@ -1,6 +1,8 @@
+use crate::ast::header::{Compression, Header};
 use crate::decode::bit_reader::SwfBitReader;
 use std::fs::File;
-use std::io::{Read, Result};
+use std::io::ErrorKind::InvalidData;
+use std::io::{Error, Read, Result};
 use std::path::Path;
 
 pub struct SwfReader<R: Read> {
@@ -18,5 +20,30 @@ impl<R: Read> SwfReader<R> {
         SwfReader {
             bit_reader: bit_reader.into(),
         }
+    }
+
+    pub fn read_header(&mut self) -> Result<Header> {
+        let mut signature = [0u8; 3];
+        self.bit_reader.read_u8_into(&mut signature)?;
+
+        let compression = match signature {
+            [0x46, 0x57, 0x54] => Ok(Compression::None),
+            [0x43, 0x57, 0x54] => Ok(Compression::Zlib),
+            [0x5a, 0x57, 0x54] => Ok(Compression::Lzma),
+            _ => Err(Error::from(InvalidData)),
+        }?;
+
+        let version = self.bit_reader.read_u8()?;
+        let frame_size = self.bit_reader.read_rectangle()?;
+        let frame_rate = self.bit_reader.read_fixed8()?; // FIXME May use a different byte order than Fixed8
+        let frame_count = self.bit_reader.read_u16()?;
+
+        Ok(Header {
+            compression,
+            version,
+            frame_size,
+            frame_rate,
+            frame_count,
+        })
     }
 }
