f2b54128c80479e057e7b121855e528acd3a7f86
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-02 18:37:13 +0000

    Add read_set_target function.

--- src/decode/tag/actions.rs
+++ src/decode/tag/actions.rs
@@ -1,4 +1,4 @@
-use crate::ast::actions::{GetUrl, GoToFrame, WaitForFrame};
+use crate::ast::actions::{GetUrl, GoToFrame, SetTarget, WaitForFrame};
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::tag_body_reader::SwfTagBodyReader;
 use std::io::{Read, Result};
@@ -18,4 +18,9 @@ fn read_wait_for_frame<R: Read>(reader: &mut SwfTagBodyReader<R>) -> Result<Wait
     let frame = reader.read_u16()?;
     let skip_count = reader.read_u8()?;
     Ok(WaitForFrame { frame, skip_count })
+}
+
+fn read_set_target<R: Read>(reader: &mut SwfTagBodyReader<R>) -> Result<SetTarget> {
+    let target_name = reader.read_string()?;
+    Ok(SetTarget { target_name })
 }
