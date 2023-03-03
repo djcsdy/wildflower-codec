dbbd0618f59bf2f5bb5ade3d1dea447734d00806
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-03 16:06:42 +0000

    Add read_fill_style function.

--- src/decode/tag/styles.rs
+++ src/decode/tag/styles.rs
@@ -1,8 +1,54 @@
-use crate::ast::styles::{FocalGradient, Gradient, GradientRecord};
+use crate::ast::styles::{FillStyle, FocalGradient, Gradient, GradientRecord};
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::tag_body_reader::SwfTagBodyReader;
 use std::io::ErrorKind::InvalidData;
 use std::io::{Error, Read, Result};
+
+pub fn read_fill_style<R: Read, Color, ReadColor: Fn(&mut SwfTagBodyReader<R>) -> Result<Color>>(
+    reader: &mut SwfTagBodyReader<R>,
+    read_color: ReadColor,
+) -> Result<FillStyle<Color>> {
+    let fill_style_type = reader.read_u8()?;
+    Ok(match fill_style_type {
+        0x00 => FillStyle::Solid(read_color(reader)?),
+        0x10 => {
+            let matrix = reader.read_matrix()?;
+            let gradient = read_gradient(reader, read_color)?;
+            FillStyle::LinearGradient { matrix, gradient }
+        }
+        0x12 => {
+            let matrix = reader.read_matrix()?;
+            let gradient = read_gradient(reader, read_color)?;
+            FillStyle::RadialGradient { matrix, gradient }
+        }
+        0x13 => {
+            let matrix = reader.read_matrix()?;
+            let gradient = read_focal_gradient(reader, read_color)?;
+            FillStyle::FocalRadialGradient { matrix, gradient }
+        }
+        0x40 => {
+            let bitmap_id = reader.read_u16()?;
+            let matrix = reader.read_matrix()?;
+            FillStyle::RepeatingBitmap { bitmap_id, matrix }
+        }
+        0x41 => {
+            let bitmap_id = reader.read_u16()?;
+            let matrix = reader.read_matrix()?;
+            FillStyle::ClippedBitmap { bitmap_id, matrix }
+        }
+        0x42 => {
+            let bitmap_id = reader.read_u16()?;
+            let matrix = reader.read_matrix()?;
+            FillStyle::NonSmoothedRepeatingBitmap { bitmap_id, matrix }
+        }
+        0x43 => {
+            let bitmap_id = reader.read_u16()?;
+            let matrix = reader.read_matrix()?;
+            FillStyle::NonSmoothedClippedBitmap { bitmap_id, matrix }
+        }
+        _ => return Err(Error::from(InvalidData)),
+    })
+}
 
 pub fn read_gradient<R: Read, Color, ReadColor: Fn(&mut SwfTagBodyReader<R>) -> Result<Color>>(
     reader: &mut SwfTagBodyReader<R>,
