f8c1584a526a6c3aaad1a0a606cb838019e19cbc
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-03 12:13:28 +0000

    Add read_frame_label_tag function.

--- src/decode/tag/control.rs
+++ src/decode/tag/control.rs
@@ -1,4 +1,5 @@
-use crate::ast::control::SetBackgroundColorTag;
+use crate::ast::control::{FrameLabelTag, SetBackgroundColorTag};
+use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::tag_body_reader::SwfTagBodyReader;
 use std::io::{Read, Result};
 
@@ -7,4 +8,10 @@ pub fn read_set_background_color_tag<R: Read>(
 ) -> Result<SetBackgroundColorTag> {
     let color = reader.read_rgb()?;
     Ok(SetBackgroundColorTag { color })
+}
+
+pub fn read_frame_label_tag<R: Read>(reader: &mut SwfTagBodyReader<R>) -> Result<FrameLabelTag> {
+    let name = reader.read_string()?;
+    let named_anchor = reader.remaining() > 0 && reader.read_u8()? == 1;
+    Ok(FrameLabelTag { name, named_anchor })
 }
