d1ba9f1addfc26e0b291a523477de00ce336f7f1
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-03 12:17:08 +0000

    Add read_export_assets_tag function.

--- src/decode/tag/control.rs
+++ src/decode/tag/control.rs
@@ -1,4 +1,6 @@
-use crate::ast::control::{FrameLabelTag, SetBackgroundColorTag};
+use crate::ast::control::{
+    ExportAssetsTag, FrameLabelTag, PortableCharacterRecord, ProtectTag, SetBackgroundColorTag,
+};
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::tag_body_reader::SwfTagBodyReader;
 use std::io::{Read, Result};
@@ -14,4 +16,23 @@ pub fn read_frame_label_tag<R: Read>(reader: &mut SwfTagBodyReader<R>) -> Result
     let name = reader.read_string()?;
     let named_anchor = reader.remaining() > 0 && reader.read_u8()? == 1;
     Ok(FrameLabelTag { name, named_anchor })
+}
+
+pub fn read_export_assets_tag<R: Read>(
+    reader: &mut SwfTagBodyReader<R>,
+) -> Result<ExportAssetsTag> {
+    let count = reader.read_u16()?;
+    let mut exports = Vec::with_capacity(count as usize);
+    for _ in 0..count {
+        exports.push(read_portable_character_record(reader)?);
+    }
+    Ok(ExportAssetsTag { exports })
+}
+
+fn read_portable_character_record<R: Read>(
+    reader: &mut SwfTagBodyReader<R>,
+) -> Result<PortableCharacterRecord> {
+    let character_id = reader.read_u16()?;
+    let name = reader.read_string()?;
+    Ok(PortableCharacterRecord { character_id, name })
 }
