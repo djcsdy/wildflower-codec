0006e9c666f4a92a3a68a7e638ad63869c21764d
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-09 18:10:45 +0000

    Add read_define_bits_jpeg2_tag function.

--- src/decode/tag/bitmaps.rs
+++ src/decode/tag/bitmaps.rs
@@ -1,4 +1,4 @@
-use crate::ast::bitmaps::{DefineBitsTag, JpegTablesTag};
+use crate::ast::bitmaps::{DefineBitsJpeg2Tag, DefineBitsTag, JpegTablesTag};
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::tag_body_reader::SwfTagBodyReader;
 use std::io::{Read, Result};
@@ -17,4 +17,16 @@ pub fn read_jpeg_tables_tag<R: Read>(reader: &mut SwfTagBodyReader<R>) -> Result
     let mut jpeg_data = Vec::new();
     reader.read_to_end(&mut jpeg_data)?;
     Ok(JpegTablesTag { jpeg_data })
+}
+
+pub fn read_define_bits_jpeg2_tag<R: Read>(
+    reader: &mut SwfTagBodyReader<R>,
+) -> Result<DefineBitsJpeg2Tag> {
+    let character_id = reader.read_u16()?;
+    let mut image_data = Vec::new();
+    reader.read_to_end(&mut image_data)?;
+    Ok(DefineBitsJpeg2Tag {
+        character_id,
+        image_data,
+    })
 }
