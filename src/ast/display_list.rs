616513066a9039669c7aab64158f45b5b158e921
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 17:15:07 +0000

    Derive Clone, PartialEq, Debug for RemoveObjectTag.

--- src/ast/display_list.rs
+++ src/ast/display_list.rs
@@ -284,6 +284,7 @@ bitflags! {
 
 /// Removes the specified character at the specified depth from the display
 /// list.
+#[derive(Clone, PartialEq, Debug)]
 pub struct RemoveObjectTag {
     /// ID of character to remove.
     pub character_id: u16,
