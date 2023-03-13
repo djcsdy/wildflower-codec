56f220cd0527b5c043506bfd865009635aa40935
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 17:15:20 +0000

    Derive Clone, PartialEq, Debug for RemoveObject2Tag.

--- src/ast/display_list.rs
+++ src/ast/display_list.rs
@@ -294,6 +294,7 @@ pub struct RemoveObjectTag {
 }
 
 /// Removes the character at the specified depth from the display list.
+#[derive(Clone, PartialEq, Debug)]
 pub struct RemoveObject2Tag {
     /// Depth of character to remove.
     pub depth: u16,
