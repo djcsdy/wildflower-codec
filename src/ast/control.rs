c4d41f7967b8a247cd5203307d393de7515d0512
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 17:49:43 +0000

    Derive Clone, PartialEq, Debug for ScriptLimitsTag.

--- src/ast/control.rs
+++ src/ast/control.rs
@@ -56,6 +56,7 @@ pub struct EnableDebugger2Tag {
 }
 
 /// Overrides the default limits for AVM scripts.
+#[derive(Clone, PartialEq, Debug)]
 pub struct ScriptLimitsTag {
     pub max_recursion_depth: u16,
     pub script_timeout_seconds: u16,
