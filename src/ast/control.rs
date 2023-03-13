fcb822fe3e44a6c134e71b1e6c9d59a750f8a539
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 17:49:59 +0000

    Derive Clone, PartialEq, Debug for SetTabIndexTag.

--- src/ast/control.rs
+++ src/ast/control.rs
@@ -63,6 +63,7 @@ pub struct ScriptLimitsTag {
 }
 
 /// Sets the tab ordering of the character at the specified depth.
+#[derive(Clone, PartialEq, Debug)]
 pub struct SetTabIndexTag {
     pub depth: u16,
     pub tab_index: u16,
