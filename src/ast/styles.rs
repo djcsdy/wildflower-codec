1d8d5c6e99a3c1b8f22d9c5bebc2ceebae18276b
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 17:57:25 +0000

    Derive Clone, Copy, PartialEq, Eq, Debug, Hash for InterpolationMode.

--- src/ast/styles.rs
+++ src/ast/styles.rs
@@ -72,7 +72,7 @@ pub enum SpreadMode {
     Repeat,
 }
 
-#[derive(IntoPrimitive, TryFromPrimitive)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
 pub enum InterpolationMode {
     Gamma,
