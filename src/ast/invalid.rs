7f6920513163a61bb224bb0a59d70ba2d7ba9361
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 16:53:48 +0000

    Derive Clone, PartialEq, Debug for InvalidTag.

--- src/ast/invalid.rs
+++ src/ast/invalid.rs
@@ -9,6 +9,7 @@ pub struct UnknownTag {
 ///
 /// The tag does not adhere to the SWF Specification, and Wildflower was unable
 /// to parse it even using known workarounds.
+#[derive(Clone, PartialEq, Debug)]
 pub struct InvalidTag {
     pub tag_code: u16,
     pub data: Vec<u8>,
