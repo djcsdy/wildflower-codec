900f0ea492f955dea6c85d53d1161d1ac89cb214
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 17:17:56 +0000

    Derive Clone, PartialEq, Debug for SoundEnvelopePoint.

--- src/ast/sounds.rs
+++ src/ast/sounds.rs
@@ -56,6 +56,7 @@ pub struct SoundInfo {
     pub envelope_points: SoundEnvelopePoint,
 }
 
+#[derive(Clone, PartialEq, Debug)]
 pub struct SoundEnvelopePoint {
     pub pos_samples_44: u32,
     pub left_level: u16,
