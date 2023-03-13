bc08adfc7c8edcc8afdc07a52e3959e0d4842122
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 18:04:05 +0000

    Derive Clone, PartialEq, Debug for StraightEdgeRecord.

--- src/ast/shapes.rs
+++ src/ast/shapes.rs
@@ -56,6 +56,7 @@ pub struct StyleChangeRecord<TColor, TLineStyle> {
     pub line_styles: Option<Vec<TLineStyle>>,
 }
 
+#[derive(Clone, PartialEq, Debug)]
 pub struct StraightEdgeRecord {
     pub delta_x: i32,
     pub delta_y: i32,
