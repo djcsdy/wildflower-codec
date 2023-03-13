3bb69c1700fe73d04429df545e08943ecc354628
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 21:35:15 +0000

    Derive Clone, PartialEq, Debug for StartSound2Tag.

--- src/ast/sounds.rs
+++ src/ast/sounds.rs
@@ -53,6 +53,7 @@ pub struct StartSoundTag {
     pub sound_info: SoundInfo,
 }
 
+#[derive(Clone, PartialEq, Debug)]
 pub struct StartSound2Tag {
     pub sound_class_name: String,
     pub sound_info: SoundInfo,
