fe15308b9471b074b0650d74ff40ee88731ec197
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 18:04:17 +0000

    Derive Clone, PartialEq, Debug for CurvedEdgeRecord.

--- src/ast/shapes.rs
+++ src/ast/shapes.rs
@@ -62,6 +62,7 @@ pub struct StraightEdgeRecord {
     pub delta_y: i32,
 }
 
+#[derive(Clone, PartialEq, Debug)]
 pub struct CurvedEdgeRecord {
     pub control_delta_x: i32,
     pub control_delta_y: i32,
