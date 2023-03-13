7385fb945730954693bd34bf0d70a9757184151c
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-13 02:43:09 +0000

    Derive Clone, PartialEq, Debug for ColorMapData.

--- src/ast/bitmaps.rs
+++ src/ast/bitmaps.rs
@@ -51,6 +51,7 @@ pub enum BitmapData<TColor> {
     Rgb(Vec<TColor>),
 }
 
+#[derive(Clone, PartialEq, Debug)]
 pub struct ColorMapData<TColor> {
     pub color_table: Vec<TColor>,
     pub pixel_data: Vec<u8>,
