fc2e84d1074a8289c3f82d49fe32b63f42d4b6c4
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-14 17:37:31 +0000

    Add read_define_font_info2_tag function.

--- src/decode/tag/fonts.rs
+++ src/decode/tag/fonts.rs
@@ -1,4 +1,6 @@
-use crate::ast::fonts::{CodeTable, DefineFontInfoTag, DefineFontTag, LanguageCode};
+use crate::ast::fonts::{
+    CodeTable, DefineFontInfo2Tag, DefineFontInfoTag, DefineFontTag, LanguageCode,
+};
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::tag::shapes::read_shape;
 use crate::decode::tag_body_reader::SwfTagBodyReader;
@@ -63,6 +65,48 @@ pub fn read_define_font_info_tag<R: Read>(
         small_text,
         italic,
         bold,
+        code_table,
+    })
+}
+
+pub fn read_define_font_info2_tag<R: Read>(
+    reader: &mut SwfTagBodyReader<R>,
+) -> Result<DefineFontInfo2Tag> {
+    let font_id = reader.read_u16()?;
+    let name_len = reader.read_u8()? as usize;
+    let font_name = reader.read_fixed_string(name_len)?;
+    reader.read_ub8(2)?;
+    let small_text = reader.read_bit()?;
+    let shift_jis = reader.read_bit()?;
+    let ansi = reader.read_bit()?;
+    let italic = reader.read_bit()?;
+    let bold = reader.read_bit()?;
+    let wide_codes = reader.read_bit()?;
+    let language_code = read_language_code(reader)?;
+    let code_table = if wide_codes {
+        if ansi {
+            return Err(Error::from(InvalidData));
+        } else if shift_jis {
+            CodeTable::ShiftJis(reader.read_u16_to_end()?)
+        } else {
+            CodeTable::Ucs2(reader.read_u16_to_end()?)
+        }
+    } else {
+        if ansi {
+            CodeTable::Windows1252(reader.read_u8_to_end()?)
+        } else if shift_jis {
+            CodeTable::JisX0201(reader.read_u8_to_end()?)
+        } else {
+            return Err(Error::from(InvalidData));
+        }
+    };
+    Ok(DefineFontInfo2Tag {
+        font_id,
+        font_name,
+        small_text,
+        italic,
+        bold,
+        language_code,
         code_table,
     })
 }
