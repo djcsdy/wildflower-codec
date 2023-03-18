9a4d37792c1b355a06bc4f316afabdd0010003b4
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-18 12:41:31 +0000

    Add ExtractTagsReader::extract_tags function.

--- src/encoded_tags/extract_tags.rs
+++ src/encoded_tags/extract_tags.rs
@@ -1,8 +1,15 @@
 use crate::ast::header::Header;
 use crate::decode::decompressing_reader::DecompressingReader;
-use std::io::BufRead;
+use crate::decode::header::read_header;
+use std::io::{BufRead, Result};
 
 pub struct ExtractTagsReader<'reader, R: BufRead> {
     reader: DecompressingReader<&'reader mut R>,
     header: Header,
+}
+
+impl<'reader, R: BufRead> ExtractTagsReader<'reader, R> {
+    pub fn extract_tags(reader: &'reader mut R) -> Result<Self> {
+        read_header(reader).map(|(header, reader)| ExtractTagsReader { reader, header })
+    }
 }
