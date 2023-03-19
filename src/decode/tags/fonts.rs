836ffb221df4c016c94a67d9eeac90807c404a2a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-19 15:39:08 +0000

    DefineFont2Flags: Correct constant names to UPPER_SNAKE_CASE.

--- src/decode/tags/fonts.rs
+++ src/decode/tags/fonts.rs
@@ -21,14 +21,14 @@ pub struct DefineFont2Tag {
 
 bitflags! {
     pub struct DefineFont2Flags: u8 {
-        const HasLayout = 0x80;
-        const ShiftJis = 0x40;
-        const SmallText = 0x20;
-        const Ansi = 0x10;
-        const WideOffsets = 0x08;
-        const WideCodes = 0x04;
-        const Italic = 0x02;
-        const Bold = 0x01;
+        const HAS_LAYOUT = 0x80;
+        const SHIFT_JIS = 0x40;
+        const SMALL_TEXT = 0x20;
+        const ANSI = 0x10;
+        const WIDE_OFFSETS = 0x08;
+        const WIDE_CODES = 0x04;
+        const ITALIC = 0x02;
+        const BOLD = 0x01;
     }
 }
 
