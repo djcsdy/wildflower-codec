588000f4e39a14894fa0362b17708daaec62dcf1
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-20 03:54:48 +0000

    Derive Clone, PartialEq, Debug for GlyphsAndLayout.

--- src/decode/tags/fonts/glyphs_and_layout.rs
+++ src/decode/tags/fonts/glyphs_and_layout.rs
@@ -1,6 +1,7 @@
 use crate::decode::tags::fonts::glyph_table::GlyphTable;
 use crate::decode::tags::fonts::layout::FontLayout;
 
+#[derive(Clone, PartialEq, Debug)]
 pub struct GlyphsAndLayout<'define_font, Offset: Copy + Into<usize>, CharacterCode> {
     pub glyph_table: GlyphTable<'define_font, Offset>,
     pub code_table: Vec<CharacterCode>,
