f376a8a1c56c82e47af43319ea8d0986c4954ca6
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-20 04:34:50 +0000

    Extract CodeTableAndLayout from GlyphsAndLayout.

--- src/decode/tags/fonts/glyphs_and_layout.rs
+++ src/decode/tags/fonts/glyphs_and_layout.rs
@@ -2,8 +2,27 @@ use crate::decode::tags::fonts::glyph_table::GlyphTable;
 use crate::decode::tags::fonts::layout::FontLayout;
 
 #[derive(Clone, PartialEq, Debug)]
-pub struct GlyphsAndLayout<'define_font, Offset: Copy + Into<usize>, CharacterCode> {
+pub struct GlyphsAndLayout<'define_font, Offset: Copy + Into<usize>> {
     pub glyph_table: GlyphTable<'define_font, Offset>,
-    pub code_table: Vec<CharacterCode>,
-    pub layout: Option<FontLayout<CharacterCode>>,
+    pub code_table_and_layout: CodeTableAndLayout,
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum CodeTableAndLayout {
+    Windows1252 {
+        character_codes: Vec<u8>,
+        layout: Option<FontLayout<u8>>,
+    },
+    JisX0201 {
+        character_codes: Vec<u8>,
+        layout: Option<FontLayout<u8>>,
+    },
+    ShiftJis {
+        character_codes: Vec<u16>,
+        layout: Option<FontLayout<u16>>,
+    },
+    Ucs2 {
+        character_codes: Vec<u16>,
+        layout: Option<FontLayout<u16>>,
+    },
 }
