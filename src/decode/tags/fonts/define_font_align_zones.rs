f4c12165f09b2c410ce56e5fa58e7804bbb2857f
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-20 15:36:50 +0000

    Add DefineFontAlignZonesTag::read function.

--- src/decode/tags/fonts/define_font_align_zones.rs
+++ src/decode/tags/fonts/define_font_align_zones.rs
@@ -1,9 +1,30 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::fonts::align_zone_record::AlignZoneRecord;
 use crate::decode::tags::fonts::csm_table_hint::CsmTableHint;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineFontAlignZonesTag {
     pub font_id: u16,
     pub csm_table_hint: CsmTableHint,
     pub zone_table: Vec<AlignZoneRecord>,
+}
+
+impl DefineFontAlignZonesTag {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let font_id = reader.read_u16()?;
+        let csm_table_hint = CsmTableHint::read(reader)?;
+        reader.read_ub8(6)?;
+        let mut zone_table = Vec::new();
+        while reader.bytes_remaining() > 0 {
+            zone_table.push(AlignZoneRecord::read(reader)?);
+        }
+        Ok(Self {
+            font_id,
+            csm_table_hint,
+            zone_table,
+        })
+    }
 }
