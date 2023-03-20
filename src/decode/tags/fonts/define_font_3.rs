ec7d978c53fa4fec71e61b5878aad6894d8f178d
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-20 14:32:45 +0000

    DefineFont3Tag: replace glyphs and layout fields with num_glyphs field and a glyphs_and_layout buffer.
    For performance, we will only partially decode this
    tag, until it is actually needed.

--- src/decode/tags/fonts/define_font_3.rs
+++ src/decode/tags/fonts/define_font_3.rs
@@ -1,9 +1,6 @@
 use crate::decode::tags::common;
-use crate::decode::tags::common::Rectangle;
 use crate::decode::tags::fonts::define_font_flags::DefineFontFlags;
 use crate::decode::tags::fonts::language_code::LanguageCode;
-use crate::decode::tags::fonts::CodeTableWithKernings;
-use crate::decode::tags::shapes::Shape;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineFont3Tag {
@@ -11,11 +8,6 @@ pub struct DefineFont3Tag {
     pub flags: DefineFontFlags,
     pub language_code: LanguageCode,
     pub font_name: common::String,
-    pub glyph_shapes: Vec<Shape<(), ()>>,
-    pub code_table: CodeTableWithKernings,
-    pub ascent: u16,
-    pub descent: u16,
-    pub leading: i16,
-    pub advances: Vec<i16>,
-    pub bounds: Vec<Rectangle>,
+    pub num_glyphs: u16,
+    pub glyphs_and_code_table_and_layout: Vec<u8>,
 }
