f91c314a06a1ca7e4ef3a918270114ec0573d496
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-20 15:41:54 +0000

    Add DefineFontNameTag::read function.

--- src/decode/tags/fonts/define_font_name.rs
+++ src/decode/tags/fonts/define_font_name.rs
@@ -1,8 +1,24 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::common;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineFontNameTag {
     pub font_id: u16,
     pub font_name: common::String,
     pub font_copyright: common::String,
+}
+
+impl DefineFontNameTag {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let font_id = reader.read_u16()?;
+        let font_name = reader.read_string()?;
+        let font_copyright = reader.read_string()?;
+        Ok(Self {
+            font_id,
+            font_name,
+            font_copyright,
+        })
+    }
 }
