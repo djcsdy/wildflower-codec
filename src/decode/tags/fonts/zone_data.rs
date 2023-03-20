5502bb008e799b2c169238ff74230f7dc6787ef8
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-20 15:19:54 +0000

    Add ZoneData::read function.

--- src/decode/tags/fonts/zone_data.rs
+++ src/decode/tags/fonts/zone_data.rs
@@ -1,7 +1,21 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use half::f16;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct ZoneData {
     pub alignment_coordinate: f16,
     pub range: f16,
+}
+
+impl ZoneData {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let alignment_coordinate = reader.read_f16()?;
+        let range = reader.read_f16()?;
+        Ok(Self {
+            alignment_coordinate,
+            range,
+        })
+    }
 }
