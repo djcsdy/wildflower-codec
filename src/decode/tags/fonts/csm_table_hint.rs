687cda34ae416fa58c7808b5239d2d58b673191a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-20 15:31:36 +0000

    Add CsmTableHint::read function.

--- src/decode/tags/fonts/csm_table_hint.rs
+++ src/decode/tags/fonts/csm_table_hint.rs
@@ -1,4 +1,7 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::slice_reader::SwfSliceReader;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::io::Result;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
@@ -6,4 +9,10 @@ pub enum CsmTableHint {
     Thin = 0,
     Medium = 1,
     Thick = 2,
+}
+
+impl CsmTableHint {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Ok(Self::try_from(reader.read_ub8(2)?).unwrap())
+    }
 }
