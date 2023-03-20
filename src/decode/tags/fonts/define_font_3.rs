23c86e111a438bd7fa40ac7475d8dc5c87ef313a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-20 14:29:46 +0000

    DefineFont3Tag: Replace individual flags with DefineFont2Flags.

--- src/decode/tags/fonts/define_font_3.rs
+++ src/decode/tags/fonts/define_font_3.rs
@@ -1,5 +1,6 @@
 use crate::decode::tags::common;
 use crate::decode::tags::common::Rectangle;
+use crate::decode::tags::fonts::define_font_2_flags::DefineFont2Flags;
 use crate::decode::tags::fonts::language_code::LanguageCode;
 use crate::decode::tags::fonts::CodeTableWithKernings;
 use crate::decode::tags::shapes::Shape;
@@ -7,9 +8,7 @@ use crate::decode::tags::shapes::Shape;
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineFont3Tag {
     pub font_id: u16,
-    pub small_text: bool,
-    pub italic: bool,
-    pub bold: bool,
+    pub flags: DefineFont2Flags,
     pub language_code: LanguageCode,
     pub font_name: common::String,
     pub glyph_shapes: Vec<Shape<(), ()>>,
