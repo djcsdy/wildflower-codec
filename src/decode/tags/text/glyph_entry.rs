e4c941ab62c1f094722050b102a6122785e07c68
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-21 10:53:56 +0000

    Add GlyphEntry::read function.

--- src/decode/tags/text/glyph_entry.rs
+++ src/decode/tags/text/glyph_entry.rs
@@ -1,4 +1,6 @@
+use crate::decode::bit_read::BitRead;
 use crate::decode::slice_reader::SwfSliceReader;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct GlyphEntry {
@@ -10,4 +12,15 @@ pub struct ReadGlyphEntryOptions<'reader, 'buffer> {
     pub reader: &'reader mut SwfSliceReader<'buffer>,
     pub glyph_bits: u8,
     pub advance_bits: u8,
+}
+
+impl GlyphEntry {
+    pub fn read(options: ReadGlyphEntryOptions) -> Result<Self> {
+        let glyph_index = options.reader.read_ub(options.glyph_bits)?;
+        let glyph_advance = options.reader.read_sb(options.advance_bits)?;
+        Ok(Self {
+            glyph_index,
+            glyph_advance,
+        })
+    }
 }
