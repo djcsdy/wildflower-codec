701216667233fbe8dd4087a65ff18fa6e7fe64a3
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-21 10:52:07 +0000

    Add ReadGlyphEntryOptions struct.

--- src/decode/tags/text/glyph_entry.rs
+++ src/decode/tags/text/glyph_entry.rs
@@ -1,5 +1,13 @@
+use crate::decode::slice_reader::SwfSliceReader;
+
 #[derive(Clone, PartialEq, Debug)]
 pub struct GlyphEntry {
     pub glyph_index: u32,
     pub glyph_advance: i32,
+}
+
+pub struct ReadGlyphEntryOptions<'reader, 'buffer> {
+    pub reader: &'reader mut SwfSliceReader<'buffer>,
+    pub glyph_bits: u8,
+    pub advance_bits: u8,
 }
