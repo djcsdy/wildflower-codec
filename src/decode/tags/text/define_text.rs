4aa2cf7653a48109634646c4627476fecd5f7987
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-21 11:33:07 +0000

    Add DefineTextTag::read function.

--- src/decode/tags/text/define_text.rs
+++ src/decode/tags/text/define_text.rs
@@ -1,5 +1,9 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
+use crate::decode::tag_readers::common::{read_matrix, read_rectangle, read_rgb};
 use crate::decode::tags::common::{Matrix, Rectangle, Rgb};
-use crate::decode::tags::text::text_record::TextRecord;
+use crate::decode::tags::text::text_record::{ReadTextRecordOptions, TextRecord};
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineTextTag {
@@ -7,4 +11,26 @@ pub struct DefineTextTag {
     pub text_bounds: Rectangle,
     pub text_matrix: Matrix,
     pub text_records: Vec<TextRecord<Rgb>>,
+}
+
+impl DefineTextTag {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let character_id = reader.read_u16()?;
+        let text_bounds = read_rectangle(reader)?;
+        let text_matrix = read_matrix(reader)?;
+        let glyph_bits = reader.read_u8()?;
+        let advance_bits = reader.read_u8()?;
+        let text_records = TextRecord::read_all(&mut ReadTextRecordOptions {
+            reader,
+            glyph_bits,
+            advance_bits,
+            read_color: &read_rgb,
+        })?;
+        Ok(Self {
+            character_id,
+            text_bounds,
+            text_matrix,
+            text_records,
+        })
+    }
 }
