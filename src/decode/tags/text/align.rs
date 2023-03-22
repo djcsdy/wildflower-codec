70058080f5b37773eaa64e1dc6968b4991f4d3ff
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-22 06:55:48 +0000

    Add Align::read function.

--- src/decode/tags/text/align.rs
+++ src/decode/tags/text/align.rs
@@ -1,4 +1,8 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::io::ErrorKind::InvalidData;
+use std::io::{Error, Result};
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
@@ -7,4 +11,10 @@ pub enum Align {
     Right = 1,
     Center = 2,
     Justify = 3,
+}
+
+impl Align {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Self::try_from(reader.read_u8()?).map_err(|_| Error::from(InvalidData))
+    }
 }
