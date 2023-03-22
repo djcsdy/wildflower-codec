5bf1db15eb05309942a23143c8f0a2a6a25ab04e
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-22 07:27:08 +0000

    Add CsmTextSettingsTag::read function.

--- src/decode/tags/text/csm_text_settings.rs
+++ src/decode/tags/text/csm_text_settings.rs
@@ -1,5 +1,9 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::text::grid_fit::GridFit;
 use crate::decode::tags::text::text_renderer::TextRenderer;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct CsmTextSettingsTag {
@@ -8,4 +12,23 @@ pub struct CsmTextSettingsTag {
     pub grid_fit: GridFit,
     pub thickness: f32,
     pub sharpness: f32,
+}
+
+impl CsmTextSettingsTag {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let text_id = reader.read_u16()?;
+        let text_renderer = TextRenderer::read(reader)?;
+        let grid_fit = GridFit::read(reader)?;
+        reader.read_ub8(3)?;
+        let thickness = reader.read_f32()?;
+        let sharpness = reader.read_f32()?;
+        reader.read_u8()?;
+        Ok(Self {
+            text_id,
+            text_renderer,
+            grid_fit,
+            thickness,
+            sharpness,
+        })
+    }
 }
