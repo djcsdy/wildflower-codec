e9e43788c1394c838f28a0ec0e6d1ef6572fd5c5
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-22 07:46:40 +0000

    Add DefineFont4Flags::read function.

--- src/decode/tags/fonts/define_font_4_flags.rs
+++ src/decode/tags/fonts/define_font_4_flags.rs
@@ -1,8 +1,18 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
+use std::io::Result;
+
 bitflags! {
     pub struct DefineFont4Flags: u8 {
         const RESERVED = 0xf8;
         const HAS_FONT_DATA = 0x04;
         const ITALIC = 0x02;
         const BOLD = 0x01;
+    }
+}
+
+impl DefineFont4Flags {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Ok(DefineFont4Flags::from_bits(reader.read_u8()?).unwrap())
     }
 }
