ae8778054e63893d70841d39bec896272387674a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-22 07:25:07 +0000

    Add GridFit::read function.

--- src/decode/tags/text/grid_fit.rs
+++ src/decode/tags/text/grid_fit.rs
@@ -1,4 +1,8 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::slice_reader::SwfSliceReader;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::io::ErrorKind::InvalidData;
+use std::io::{Error, Result};
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
@@ -6,4 +10,10 @@ pub enum GridFit {
     None = 0,
     PixelFit = 1,
     SubPixelFit = 2,
+}
+
+impl GridFit {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Self::try_from(reader.read_ub8(3)?).map_err(|_| Error::from(InvalidData))
+    }
 }
