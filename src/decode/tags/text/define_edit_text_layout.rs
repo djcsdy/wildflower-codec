7adb988671f588e8409bdba9340774c810105803
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-22 06:57:24 +0000

    Add DefineEditTextLayout::read function.

--- src/decode/tags/text/define_edit_text_layout.rs
+++ src/decode/tags/text/define_edit_text_layout.rs
@@ -1,4 +1,7 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::text::align::Align;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineEditTextLayout {
@@ -7,4 +10,21 @@ pub struct DefineEditTextLayout {
     pub right_margin: u16,
     pub indent: u16,
     pub leading: i16,
+}
+
+impl DefineEditTextLayout {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let align = Align::read(reader)?;
+        let left_margin = reader.read_u16()?;
+        let right_margin = reader.read_u16()?;
+        let indent = reader.read_u16()?;
+        let leading = reader.read_i16()?;
+        Ok(Self {
+            align,
+            left_margin,
+            right_margin,
+            indent,
+            leading,
+        })
+    }
 }
