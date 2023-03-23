1fc53dbd0c0818d996aeb2b6ff3f2284b04887a8
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-23 16:38:32 +0000

    SoundInfo: Correct type of envelope_points to Vec<SoundEnvelopePoint>.

--- src/decode/tags/sounds/sound_info.rs
+++ src/decode/tags/sounds/sound_info.rs
@@ -7,5 +7,5 @@ pub struct SoundInfo {
     pub in_point: u32,
     pub out_point: u32,
     pub loop_count: u16,
-    pub envelope_points: SoundEnvelopePoint,
+    pub envelope_points: Vec<SoundEnvelopePoint>,
 }
