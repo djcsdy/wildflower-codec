602967c2dee0fbf1c379f5bf8efbed678500a78d
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-23 16:07:04 +0000

    Add SoundFormat::read function.

--- src/decode/tags/sounds/sound_format.rs
+++ src/decode/tags/sounds/sound_format.rs
@@ -1,4 +1,8 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::slice_reader::SwfSliceReader;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::io::ErrorKind::InvalidData;
+use std::io::{Error, Result};
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
@@ -11,4 +15,10 @@ pub enum SoundFormat {
     Nellymoser8Khz = 5,
     Nellymoser = 6,
     Speex = 11,
+}
+
+impl SoundFormat {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        SoundFormat::try_from(reader.read_ub8(4)?).map_err(|_| Error::from(InvalidData))
+    }
 }
