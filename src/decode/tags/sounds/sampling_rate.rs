44d76a13951ddb35ecb6d3d281f11f92599ef32e
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-23 16:09:34 +0000

    Add SamplingRate::read function.

--- src/decode/tags/sounds/sampling_rate.rs
+++ src/decode/tags/sounds/sampling_rate.rs
@@ -1,4 +1,7 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::slice_reader::SwfSliceReader;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::io::Result;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
@@ -7,4 +10,10 @@ pub enum SamplingRate {
     Khz11 = 1,
     Khz22 = 2,
     Khz44 = 3,
+}
+
+impl SamplingRate {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Ok(Self::try_from(reader.read_ub8(2)?).unwrap())
+    }
 }
