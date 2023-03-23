3c495ec8190f0dd6aff7a5bfbc34b72ace938a12
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-23 16:43:28 +0000

    Add SoundInfo::read function.

--- src/decode/tags/sounds/sound_info.rs
+++ src/decode/tags/sounds/sound_info.rs
@@ -1,5 +1,8 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::sounds::sound_envelope_point::SoundEnvelopePoint;
 use crate::decode::tags::sounds::sound_info_flags::SoundInfoFlags;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct SoundInfo {
@@ -8,4 +11,41 @@ pub struct SoundInfo {
     pub out_point: Option<u32>,
     pub loop_count: u16,
     pub envelope_points: Vec<SoundEnvelopePoint>,
+}
+
+impl SoundInfo {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let flags = SoundInfoFlags::read(reader)?;
+        let in_point = if flags.contains(SoundInfoFlags::HAS_IN_POINT) {
+            reader.read_u32()?
+        } else {
+            0
+        };
+        let out_point = if flags.contains(SoundInfoFlags::HAS_OUT_POINT) {
+            Some(reader.read_u32()?)
+        } else {
+            None
+        };
+        let loop_count = if flags.contains(SoundInfoFlags::HAS_LOOPS) {
+            reader.read_u16()?
+        } else {
+            1
+        };
+        let envelope_point_count = if flags.contains(SoundInfoFlags::HAS_ENVELOPE) {
+            reader.read_u8()?
+        } else {
+            0
+        };
+        let mut envelope_points = Vec::with_capacity(envelope_point_count as usize);
+        for _ in 0..envelope_point_count {
+            envelope_points.push(SoundEnvelopePoint::read(reader)?);
+        }
+        Ok(Self {
+            flags,
+            in_point,
+            out_point,
+            loop_count,
+            envelope_points,
+        })
+    }
 }
