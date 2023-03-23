8b96f9ea3c4a2f6859777e796379cde23dd54581
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-23 17:23:20 +0000

    SoundStreamHead2Tag: Add missing playback_bits field.

--- src/decode/tags/sounds/sound_stream_head_2.rs
+++ src/decode/tags/sounds/sound_stream_head_2.rs
@@ -6,6 +6,7 @@ use crate::decode::tags::sounds::sound_format::SoundFormat;
 #[derive(Clone, PartialEq, Debug)]
 pub struct SoundStreamHead2Tag {
     pub playback_sound_rate: SamplingRate,
+    pub playback_bits: SamplingBits,
     pub playback_channels: SoundChannels,
     pub stream_sound_compression: SoundFormat,
     pub stream_sound_rate: SamplingRate,
