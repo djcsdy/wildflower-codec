406514f2c1a0981be97c081ff12ccb4bbed1b2b7
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-23 17:27:37 +0000

    Add SoundStreamBlockTag::read function.

--- src/decode/tags/sounds/sound_stream_block.rs
+++ src/decode/tags/sounds/sound_stream_block.rs
@@ -1,4 +1,15 @@
+use crate::decode::slice_reader::SwfSliceReader;
+use std::io::Result;
+
 #[derive(Clone, PartialEq, Debug)]
 pub struct SoundStreamBlockTag {
     pub stream_sound_data: Vec<u8>,
+}
+
+impl SoundStreamBlockTag {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Ok(Self {
+            stream_sound_data: reader.read_u8_to_end()?,
+        })
+    }
 }
