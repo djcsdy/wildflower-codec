f5ed5e4aabe7f0fad97e04bde928fc846856d95e
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-23 16:52:21 +0000

    Add SoundStreamCompression::read function.

--- src/decode/tags/sounds/sound_stream_compression.rs
+++ src/decode/tags/sounds/sound_stream_compression.rs
@@ -1,8 +1,18 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::slice_reader::SwfSliceReader;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::io::ErrorKind::InvalidData;
+use std::io::{Error, Result};
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
 pub enum SoundStreamCompression {
     Adpcm = 1,
     Mp3 = 2,
+}
+
+impl SoundStreamCompression {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Self::try_from(reader.read_ub8(2)?).map_err(|_| Error::from(InvalidData))
+    }
 }
