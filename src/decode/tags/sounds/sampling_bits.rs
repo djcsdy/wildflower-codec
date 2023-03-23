bcb8958bc536f4760e6e6217f6a10e80a79b02e7
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-23 16:13:51 +0000

    Add SamplingBits::read function.

--- src/decode/tags/sounds/sampling_bits.rs
+++ src/decode/tags/sounds/sampling_bits.rs
@@ -1,8 +1,17 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::slice_reader::SwfSliceReader;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::io::Result;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
 pub enum SamplingBits {
     Bits8 = 0,
     Bits16 = 1,
+}
+
+impl SamplingBits {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Ok(Self::try_from(reader.read_ub8(1)?).unwrap())
+    }
 }
