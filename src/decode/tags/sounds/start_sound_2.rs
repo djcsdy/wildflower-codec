756f6baf64d923413c78c67e7bf53683cae6ecd3
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-23 16:46:09 +0000

    Add StartSoundTag::read function.

--- src/decode/tags/sounds/start_sound_2.rs
+++ src/decode/tags/sounds/start_sound_2.rs
@@ -1,8 +1,21 @@
+use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::common::string::String;
 use crate::decode::tags::sounds::sound_info::SoundInfo;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct StartSound2Tag {
     pub sound_class_name: String,
     pub sound_info: SoundInfo,
+}
+
+impl StartSound2Tag {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let sound_class_name = String::read(reader)?;
+        let sound_info = SoundInfo::read(reader)?;
+        Ok(Self {
+            sound_class_name,
+            sound_info,
+        })
+    }
 }
