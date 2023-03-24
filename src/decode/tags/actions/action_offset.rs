fd3c087e67d00777f9ad2f5ede0e0ee40ce88f01
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-24 17:53:14 +0000

    Implement From<usize> for ActionOffset.

--- src/decode/tags/actions/action_offset.rs
+++ src/decode/tags/actions/action_offset.rs
@@ -1,2 +1,8 @@
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
 pub struct ActionOffset(usize);
+
+impl From<usize> for ActionOffset {
+    fn from(value: usize) -> Self {
+        Self(value)
+    }
+}
