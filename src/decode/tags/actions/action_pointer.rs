9202a062ef0fa719cf552e9f27e046b9e340c408
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-24 17:50:39 +0000

    Add ActionPointer::START constant.

--- src/decode/tags/actions/action_pointer.rs
+++ src/decode/tags/actions/action_pointer.rs
@@ -3,6 +3,8 @@
 pub struct ActionPointer(usize);
 
 impl ActionPointer {
+    pub const START: Self = ActionPointer(0);
+
     pub fn index_buffer<'buffer, Buffer: Into<&'buffer [u8]>>(
         &self,
         buffer: Buffer,
