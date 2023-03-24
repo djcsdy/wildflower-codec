47f7c4a089aba0c1a07b2a16194395a7d8615a46
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-24 17:54:56 +0000

    Add ActionOffset::offset function.

--- src/decode/tags/actions/action_offset.rs
+++ src/decode/tags/actions/action_offset.rs
@@ -1,6 +1,12 @@
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
 pub struct ActionOffset(usize);
 
+impl ActionOffset {
+    pub fn offset(&self) -> usize {
+        self.0
+    }
+}
+
 impl From<usize> for ActionOffset {
     fn from(value: usize) -> Self {
         Self(value)
