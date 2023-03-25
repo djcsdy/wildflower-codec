0448fd0787b23426e6e4f53d9a21e64ffd64f665
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-25 16:32:01 +0000

    Add DefineButtonTag::read function.

--- src/decode/tags/buttons/define_button.rs
+++ src/decode/tags/buttons/define_button.rs
@@ -1,9 +1,25 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::actions::action_list::ActionList;
 use crate::decode::tags::buttons::button_record::ButtonRecord;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineButtonTag {
     pub button_id: u16,
     pub characters: Vec<ButtonRecord>,
     pub actions: ActionList<Vec<u8>>,
+}
+
+impl DefineButtonTag {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let button_id = reader.read_u16()?;
+        let characters = ButtonRecord::read_list(reader)?;
+        let actions = ActionList::read_to_end(reader)?;
+        Ok(Self {
+            button_id,
+            characters,
+            actions,
+        })
+    }
 }
