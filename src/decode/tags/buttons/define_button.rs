d31afb3b81d03174f103222fe29147b77731f2bc
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-25 15:36:14 +0000

    DefineButtonTag: Change type of actions to ActionList.

--- src/decode/tags/buttons/define_button.rs
+++ src/decode/tags/buttons/define_button.rs
@@ -1,9 +1,9 @@
-use crate::decode::tags::actions::action_record::ActionRecord;
+use crate::decode::tags::actions::action_list::ActionList;
 use crate::decode::tags::buttons::button_record::ButtonRecord;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineButtonTag {
     pub button_id: u16,
     pub characters: Vec<ButtonRecord>,
-    pub actions: Vec<ActionRecord>,
+    pub actions: ActionList<Vec<u8>>,
 }
