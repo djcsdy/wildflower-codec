e93139d317e1d5f3e94a6ae6b1118bfa11ab20b9
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-25 10:43:18 +0000

    Extract Flags struct from Try::read function.

--- src/decode/tags/actions/try.rs
+++ src/decode/tags/actions/try.rs
@@ -1,4 +1,3 @@
-use crate::decode::bit_read::BitRead;
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::actions::action_list::ActionList;
@@ -14,27 +13,39 @@ pub struct Try {
     pub finally_body: Option<ActionList<Vec<u8>>>,
 }
 
+bitflags! {
+    struct Flags: u8 {
+        const RESERVED = 0xf8;
+        const CATCH_IN_REGISTER = 0x04;
+        const HAS_FINALLY_BLOCK = 0x02;
+        const HAS_CATCH_BLOCK = 0x01;
+    }
+}
+
+impl Flags {
+    fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Ok(Self::from_bits(reader.read_u8()?).unwrap())
+    }
+}
+
 impl Try {
     pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
-        reader.read_ub8(5)?;
-        let catch_in_register = reader.read_bit()?;
-        let has_finally_block = reader.read_bit()?;
-        let has_catch_block = reader.read_bit()?;
+        let flags = Flags::read(reader)?;
         let try_size = reader.read_u16()?;
         let catch_size = reader.read_u16()?;
         let finally_size = reader.read_u16()?;
-        let catch_parameter = if catch_in_register {
+        let catch_parameter = if flags.contains(Flags::CATCH_IN_REGISTER) {
             RegisterParam::Register(reader.read_u8()?)
         } else {
             RegisterParam::Name(string::String::read(reader)?)
         };
         let try_body = ActionList::read(reader, try_size as usize)?;
-        let catch_body = if has_catch_block {
+        let catch_body = if flags.contains(Flags::HAS_CATCH_BLOCK) {
             Some(ActionList::read(reader, catch_size as usize)?)
         } else {
             None
         };
-        let finally_body = if has_finally_block {
+        let finally_body = if flags.contains(Flags::HAS_FINALLY_BLOCK) {
             Some(ActionList::read(reader, finally_size as usize)?)
         } else {
             None
