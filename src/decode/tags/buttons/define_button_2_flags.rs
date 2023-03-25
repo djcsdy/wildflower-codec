42cc9ac26601c60c21de2d4ebd06becdc91c364b
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-25 17:13:05 +0000

    Add DefineButton2Flags::read function.

--- src/decode/tags/buttons/define_button_2_flags.rs
+++ src/decode/tags/buttons/define_button_2_flags.rs
@@ -1,6 +1,16 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
+use std::io::Result;
+
 bitflags! {
     pub struct DefineButton2Flags: u8 {
         const RESERVED = 0xfe;
         const TRACK_AS_MENU = 0x01;
+    }
+}
+
+impl DefineButton2Flags {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        Ok(DefineButton2Flags::from_bits(reader.read_u8()?).unwrap())
     }
 }
