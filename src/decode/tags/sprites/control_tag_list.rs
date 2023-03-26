41892905ced3561419c346e5134716b34c9b6d24
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 14:48:24 +0100

    Add ControlTagList::read_to_end function.

--- src/decode/tags/sprites/control_tag_list.rs
+++ src/decode/tags/sprites/control_tag_list.rs
@@ -1,3 +1,13 @@
+use crate::decode::slice_reader::SwfSliceReader;
+use std::io::Result;
+
 pub struct ControlTagList<Buffer: AsRef<[u8]>> {
     pub buffer: Buffer,
+}
+
+impl ControlTagList<Vec<u8>> {
+    pub fn read_to_end(reader: &mut SwfSliceReader) -> Result<Self> {
+        let buffer = reader.read_u8_to_end()?;
+        Ok(Self { buffer })
+    }
 }
