630585bf3766562c7283850eddae3f631e61b9c8
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 16:40:58 +0100

    Derive Clone, PartialEq, Eq, Debug, Hash for SwfBlock.

--- src/buffer/block.rs
+++ src/buffer/block.rs
@@ -4,6 +4,7 @@ const BLOCK_SIZE: usize = 1 << 15;
 ///
 /// This block size is chosen because it is the next larger power of 2
 /// than the maximum offset of an AVM1 branch instruction.
+#[derive(Clone, PartialEq, Eq, Debug, Hash)]
 pub struct SwfBlock {
     pub bytes: [u8; BLOCK_SIZE],
 }
