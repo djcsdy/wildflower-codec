cee0e566675e347146525ef3b4f01b6ed943049f
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 02:27:21 +0100

    Add ButtonSound::read function.

--- src/decode/tags/buttons/button_sound.rs
+++ src/decode/tags/buttons/button_sound.rs
@@ -1,7 +1,25 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::sounds::sound_info::SoundInfo;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct ButtonSound {
     pub sound_id: u16,
     pub sound_info: SoundInfo,
+}
+
+impl ButtonSound {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Option<Self>> {
+        let sound_id = reader.read_u16()?;
+        Ok(if sound_id == 0 {
+            None
+        } else {
+            let sound_info = SoundInfo::read(reader)?;
+            Some(Self {
+                sound_id,
+                sound_info,
+            })
+        })
+    }
 }
