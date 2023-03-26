5e97efca8fb8ea1d757c3b4b3c464f76738ec797
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 16:38:15 +0100

    Add SwfOffset struct.

--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -1,3 +1,4 @@
 pub mod block;
 pub mod block_index;
+pub mod offset;
 pub mod pointer;
---
+++ src/buffer/offset.rs
@@ -0,0 +1,2 @@
+/// A byte-offset from a [SwfPointer][super::pointer::SwfPointer].
+pub struct SwfOffset(i32);
