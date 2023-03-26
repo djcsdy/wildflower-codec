a786a75203c947fdbfad54f56a2c195c7f8e6c1a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 16:39:14 +0100

    Implement Add for SwfOffset.

--- src/buffer/offset.rs
+++ src/buffer/offset.rs
@@ -1,2 +1,12 @@
+use std::ops::Add;
+
 /// A byte-offset from a [SwfPointer][super::pointer::SwfPointer].
 pub struct SwfOffset(i32);
+
+impl Add for SwfOffset {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self(self.0 + rhs.0)
+    }
+}
