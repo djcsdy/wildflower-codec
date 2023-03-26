f2b102be51f36fcbc698071be5bb3dfdcdee6cf3
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 16:29:03 +0100

    Add SwfBlockIndex struct.

---
+++ src/buffer/block_index.rs
@@ -0,0 +1,17 @@
+/// An index into the list of [SwfBlock][super::block::SwfBlock]s contained by
+/// a SWF file.
+///
+/// [ZERO][SwfBlockIndex::ZERO] refers to the first SwfBlock in a SWF file.
+///
+/// The first SwfBlock starts at the first byte after the end of the
+/// [Header][crate::decode::tags::header::Header].
+pub struct SwfBlockIndex(u32);
+
+impl SwfBlockIndex {
+    /// The index of the first [SwfBlock][super::block::SwfBlock] in a SWF
+    /// file.
+    ///
+    /// The first SwfBlock starts at the first byte after the end of the
+    /// [Header][crate::decode::tags::header::Header].
+    pub const ZERO: Self = Self(0);
+}
