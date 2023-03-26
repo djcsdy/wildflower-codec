ead7a03c636889c335a21f9362a6460db384bd25
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 15:40:05 +0100

    DefineSpriteTag: Change type of control_tags to ControlTagList<Vec<u8>>.

--- src/decode/tags/sprites/define_sprite.rs
+++ src/decode/tags/sprites/define_sprite.rs
@@ -1,8 +1,8 @@
-use crate::decode::tags::sprites::control_tag::ControlTag;
+use crate::decode::tags::sprites::control_tag_list::ControlTagList;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineSpriteTag {
     pub sprite_id: u16,
     pub frame_count: u16,
-    pub control_tags: Vec<ControlTag>,
+    pub control_tags: ControlTagList<Vec<u8>>
 }
