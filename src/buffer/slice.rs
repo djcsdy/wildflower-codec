8fed4302767649629bdbc9e2857032ec04e8aff5
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 17:31:41 +0100

    Add SwfSlice::new struct.

--- src/buffer/slice.rs
+++ src/buffer/slice.rs
@@ -9,3 +9,19 @@ pub struct SwfSlice {
     start_offset: SwfOffset,
     end_offset: SwfOffset,
 }
+
+impl SwfSlice {
+    pub(super) fn new(
+        start_pointer: SwfPointer,
+        blocks: Vec<Arc<SwfBlock>>,
+        start_offset: SwfOffset,
+        end_offset: SwfOffset,
+    ) -> Self {
+        Self {
+            start_pointer,
+            blocks,
+            start_offset,
+            end_offset,
+        }
+    }
+}
