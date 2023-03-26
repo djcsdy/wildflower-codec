417b06b6f28a1bfc0d805148b95b8fe67473b02d
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 16:49:39 +0100

    Implement Add<SwfOffset> for SwfPointer.

--- src/buffer/offset.rs
+++ src/buffer/offset.rs
@@ -2,7 +2,7 @@ use std::ops::{Add, Sub};
 
 /// A byte-offset from a [SwfPointer][super::pointer::SwfPointer].
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
-pub struct SwfOffset(i32);
+pub struct SwfOffset(pub(super) i32);
 
 impl Add for SwfOffset {
     type Output = Self;
--- src/buffer/pointer.rs
+++ src/buffer/pointer.rs
@@ -1,3 +1,6 @@
+use crate::buffer::offset::SwfOffset;
+use std::ops::Add;
+
 /// A pointer to a specific byte within a SWF file.
 ///
 /// [ZERO][SwfPointer::ZERO] refers to the first byte of the SWF file after
@@ -9,4 +12,12 @@ impl SwfPointer {
     /// A pointer to the first byte of a SWF file after the
     /// [Header][crate::decode::tags::header::Header].
     pub const ZERO: Self = Self(0);
+}
+
+impl Add<SwfOffset> for SwfPointer {
+    type Output = Self;
+
+    fn add(self, rhs: SwfOffset) -> Self {
+        Self(self.0.checked_add_signed(rhs.0).unwrap())
+    }
 }
