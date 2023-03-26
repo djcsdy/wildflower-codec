bda73f30262821c73914947529a1d273464f895b
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-26 02:14:13 +0100

    Add DefineButtonColorTransformTag::read function.

--- src/decode/tags/buttons/define_button_color_transform.rs
+++ src/decode/tags/buttons/define_button_color_transform.rs
@@ -1,7 +1,21 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::common::color_transform::ColorTransform;
+use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DefineButtonColorTransformTag {
     pub button_id: u16,
     pub button_color_transform: ColorTransform,
+}
+
+impl DefineButtonColorTransformTag {
+    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+        let button_id = reader.read_u16()?;
+        let button_color_transform = ColorTransform::read(reader)?;
+        Ok(Self {
+            button_id,
+            button_color_transform,
+        })
+    }
 }
