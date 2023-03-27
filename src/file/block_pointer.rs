87ce5dd81cdf66b6cab26e30eeac37a330eb95f7
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-27 16:37:10 +0100

    Implement Into<usize> for SwfBlockPointer.

--- src/file/block_pointer.rs
+++ src/file/block_pointer.rs
@@ -1,3 +1,9 @@
 /// A pointer to a specific byte within a [SwfBlock][super::block::SwfBlock].
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
 pub struct SwfBlockPointer(pub(super) u32);
+
+impl Into<usize> for SwfBlockPointer {
+    fn into(self) -> usize {
+        self.0 as usize
+    }
+}
