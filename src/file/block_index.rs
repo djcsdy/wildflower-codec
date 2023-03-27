82f6264ad96b95b4c61b7884d68e8df1fc572f57
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-27 15:25:29 +0100

    Add SwfBlockIndex::of_pointer function.

--- src/file/block_index.rs
+++ src/file/block_index.rs
@@ -23,4 +23,10 @@ impl SwfBlockIndex {
     pub(super) fn as_pointer(&self) -> SwfPointer {
         SwfPointer(self.0 * BLOCK_SIZE as u32)
     }
+
+    /// Returns the [SwfBlockIndex] of the [SwfBlock][super::block::SwfBlock]
+    /// that contains the byte pointed to by the specified [SwfPointer].
+    pub(super) fn of_pointer(pointer: SwfPointer) -> SwfBlockIndex {
+        SwfBlockIndex((pointer.0 as usize / BLOCK_SIZE) as u32)
+    }
 }
