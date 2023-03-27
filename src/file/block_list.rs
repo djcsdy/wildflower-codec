cf74f8532bd3de66eade804174cb93001da9c5be
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-27 15:38:13 +0100

    Add SwfBlockList struct.

---
+++ src/file/block_list.rs
@@ -0,0 +1,4 @@
+use crate::file::block::SwfBlock;
+
+#[derive(Clone, Debug)]
+pub(super) struct SwfBlockList(Vec<SwfBlock>);
--- src/file/mod.rs
+++ src/file/mod.rs
@@ -1,5 +1,6 @@
 mod block;
 mod block_index;
+mod block_list;
 pub mod file;
 pub mod offset;
 pub mod pointer;
