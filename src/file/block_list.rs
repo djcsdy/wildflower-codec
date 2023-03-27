a75c1a01f536a0131a5059a923ff1d1c61d3d449
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-27 15:44:14 +0100

    Add SwfBlockList::read function.

--- src/file/block_list.rs
+++ src/file/block_list.rs
@@ -1,4 +1,16 @@
-use crate::file::block::SwfBlock;
+use crate::file::block::{SwfBlock, BLOCK_SIZE};
+use std::io::{Read, Result};
 
 #[derive(Clone, Debug)]
 pub(super) struct SwfBlockList(Vec<SwfBlock>);
+
+impl SwfBlockList {
+    pub fn read<R: Read>(reader: &mut R, length: u32) -> Result<Self> {
+        let block_count = length as usize / BLOCK_SIZE;
+        let mut blocks = Vec::with_capacity(block_count);
+        for _ in 0..block_count {
+            blocks.push(SwfBlock::read(reader)?);
+        }
+        Ok(Self(blocks))
+    }
+}
