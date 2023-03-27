a7afd9e46280c8593b209bbfa849cd0d5bc5323c
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-03-27 03:26:04 +0100

    Add SwfFile::read function.

--- src/file/file.rs
+++ src/file/file.rs
@@ -1,6 +1,16 @@
 use crate::decode::header::Header;
+use crate::decode::read_ext::SwfTypesReadExt;
+use std::io::{BufRead, Result};
 
 pub struct SwfFile {
     header: Header,
     payload: Vec<u8>,
+}
+
+impl SwfFile {
+    pub fn read<R: BufRead>(reader: R) -> Result<Self> {
+        let (header, mut reader) = Header::read(reader)?;
+        let payload = reader.read_u8_to_end()?;
+        Ok(Self { header, payload })
+    }
 }
