d16c0c0e350a07b66950cfbf79f2dccc937ea2c1
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-08 04:06:36 +0100

    read_pix15: Accept any BitRead as argument.

--- src/decode/tags/bitmaps/pix15.rs
+++ src/decode/tags/bitmaps/pix15.rs
@@ -1,9 +1,8 @@
 use crate::decode::bit_read::BitRead;
-use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::common::rgb::Rgb;
 use std::io::Result;
 
-pub(crate) fn read_pix15(reader: &mut SwfSliceReader) -> Result<Rgb> {
+pub(crate) fn read_pix15<R: BitRead>(reader: &mut R) -> Result<Rgb> {
     reader.read_bit()?;
     let red = reader.read_ub8(5)? << 3;
     let green = reader.read_ub8(5)? << 3;
