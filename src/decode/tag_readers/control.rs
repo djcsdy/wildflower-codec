cce0cdbeba4331b382f513012896b1233bb01298
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-08 06:45:26 +0100

    read_frame_label_tag: Accept any SizedRead as argument.

--- src/decode/tag_readers/control.rs
+++ src/decode/tag_readers/control.rs
@@ -1,11 +1,10 @@
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::sized_read::SizedRead;
-use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::common::string::String;
 use crate::decode::tags::control::frame_label::FrameLabelTag;
 use std::io::Result;
 
-pub fn read_frame_label_tag(reader: &mut SwfSliceReader) -> Result<FrameLabelTag> {
+pub fn read_frame_label_tag<R: SizedRead>(reader: &mut R) -> Result<FrameLabelTag> {
     let name = String::read(reader)?;
     let named_anchor = reader.remaining_bytes() > 0 && reader.read_u8()? == 1;
     Ok(FrameLabelTag { name, named_anchor })
