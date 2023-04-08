1e501ebec87b210f18576168f8752b1bd3514edd
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-08 03:47:03 +0100

    Add SwfVersion::read function.

--- src/decode/swf_version.rs
+++ src/decode/swf_version.rs
@@ -1,2 +1,11 @@
+use crate::decode::read_ext::SwfTypesReadExt;
+use std::io::{Read, Result};
+
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
 pub struct SwfVersion(pub u8);
+
+impl SwfVersion {
+    pub fn read<R: Read>(reader: &mut R) -> Result<Self> {
+        Ok(Self(reader.read_u8()?))
+    }
+}
