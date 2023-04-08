41aad196f3c01a36427f7b46cda9525decd28e47
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-08 05:08:05 +0100

    Move read_morph_line_style_2 function to MorphLineStyle2::read.

--- src/decode/tag_readers/shape_morphing.rs
+++ src/decode/tag_readers/shape_morphing.rs
@@ -2,19 +2,14 @@ use crate::decode::bit_read::BitRead;
 use crate::decode::read_ext::SwfTypesReadExt;
 use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tag_readers::shapes::read_shape;
-use crate::decode::tags::common::fixed_8::Fixed8;
 use crate::decode::tags::common::rectangle::Rectangle;
-use crate::decode::tags::common::rgba::Rgba;
 use crate::decode::tags::shape_morphing::define_morph_shape::DefineMorphShapeTag;
 use crate::decode::tags::shape_morphing::define_morph_shape_2::DefineMorphShape2Tag;
 use crate::decode::tags::shape_morphing::morph_fill_style::MorphFillStyle;
 use crate::decode::tags::shape_morphing::morph_line_style::MorphLineStyle;
 use crate::decode::tags::shape_morphing::morph_line_style_2::MorphLineStyle2;
-use crate::decode::tags::styles::cap_style::CapStyle;
-use crate::decode::tags::styles::join_style::JoinStyle;
 use crate::decode::tags::styles::line_style_array::read_line_style_array;
-use std::io::ErrorKind::InvalidData;
-use std::io::{Error, Result};
+use std::io::Result;
 
 pub fn read_define_morph_shape_tag(reader: &mut SwfSliceReader) -> Result<DefineMorphShapeTag> {
     let character_id = reader.read_u16()?;
@@ -47,7 +42,7 @@ pub fn read_define_morph_shape_2_tag(reader: &mut SwfSliceReader) -> Result<Defi
     let uses_scaling_strokes = reader.read_bit()?;
     let offset = reader.read_u32()? as usize;
     let fill_styles = MorphFillStyle::read_array(reader)?;
-    let line_styles = read_line_style_array(reader, &read_morph_line_style_2)?;
+    let line_styles = read_line_style_array(reader, &MorphLineStyle2::read)?;
     let start_edges = read_shape(&mut reader.slice(offset))?;
     let end_edges = read_shape(reader)?;
     Ok(DefineMorphShape2Tag {
@@ -62,53 +57,5 @@ pub fn read_define_morph_shape_2_tag(reader: &mut SwfSliceReader) -> Result<Defi
         line_styles,
         start_edges,
         end_edges,
-    })
-}
-
-fn read_morph_line_style_2<R: BitRead>(reader: &mut R) -> Result<MorphLineStyle2> {
-    let start_width = reader.read_u16()?;
-    let end_width = reader.read_u16()?;
-    let start_cap_style = CapStyle::read(reader)?;
-    let join_style = reader.read_ub8(2)?;
-    let has_fill = reader.read_bit()?;
-    let no_h_scale = reader.read_bit()?;
-    let no_v_scale = reader.read_bit()?;
-    let pixel_hinting = reader.read_bit()?;
-    reader.read_ub8(5)?;
-    let no_close = reader.read_bit()?;
-    let end_cap_style = CapStyle::read(reader)?;
-    let miter_limit_factor = if join_style == 2 {
-        Some(Fixed8::read(reader)?)
-    } else {
-        None
-    };
-    let fill_style = if has_fill {
-        MorphFillStyle::read(reader)?
-    } else {
-        let start_color = Rgba::read(reader)?;
-        let end_color = Rgba::read(reader)?;
-        MorphFillStyle::Solid {
-            start_color,
-            end_color,
-        }
-    };
-    Ok(MorphLineStyle2 {
-        start_width,
-        end_width,
-        start_cap_style,
-        join_style: match join_style {
-            0 => JoinStyle::Round,
-            1 => JoinStyle::Bevel,
-            2 => JoinStyle::Miter {
-                miter_limit_factor: miter_limit_factor.unwrap(),
-            },
-            _ => return Err(Error::from(InvalidData)),
-        },
-        no_h_scale,
-        no_v_scale,
-        pixel_hinting,
-        no_close,
-        end_cap_style,
-        fill_style,
     })
 }
--- src/decode/tags/shape_morphing/morph_line_style_2.rs
+++ src/decode/tags/shape_morphing/morph_line_style_2.rs
@@ -1,6 +1,12 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::tags::common::fixed_8::Fixed8;
+use crate::decode::tags::common::rgba::Rgba;
 use crate::decode::tags::shape_morphing::morph_fill_style::MorphFillStyle;
 use crate::decode::tags::styles::cap_style::CapStyle;
 use crate::decode::tags::styles::join_style::JoinStyle;
+use std::io::ErrorKind::InvalidData;
+use std::io::{Error, Result};
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct MorphLineStyle2 {
@@ -14,4 +20,54 @@ pub struct MorphLineStyle2 {
     pub no_close: bool,
     pub end_cap_style: CapStyle,
     pub fill_style: MorphFillStyle,
+}
+
+impl MorphLineStyle2 {
+    pub fn read<R: BitRead>(reader: &mut R) -> Result<Self> {
+        let start_width = reader.read_u16()?;
+        let end_width = reader.read_u16()?;
+        let start_cap_style = CapStyle::read(reader)?;
+        let join_style = reader.read_ub8(2)?;
+        let has_fill = reader.read_bit()?;
+        let no_h_scale = reader.read_bit()?;
+        let no_v_scale = reader.read_bit()?;
+        let pixel_hinting = reader.read_bit()?;
+        reader.read_ub8(5)?;
+        let no_close = reader.read_bit()?;
+        let end_cap_style = CapStyle::read(reader)?;
+        let miter_limit_factor = if join_style == 2 {
+            Some(Fixed8::read(reader)?)
+        } else {
+            None
+        };
+        let fill_style = if has_fill {
+            MorphFillStyle::read(reader)?
+        } else {
+            let start_color = Rgba::read(reader)?;
+            let end_color = Rgba::read(reader)?;
+            MorphFillStyle::Solid {
+                start_color,
+                end_color,
+            }
+        };
+        Ok(Self {
+            start_width,
+            end_width,
+            start_cap_style,
+            join_style: match join_style {
+                0 => JoinStyle::Round,
+                1 => JoinStyle::Bevel,
+                2 => JoinStyle::Miter {
+                    miter_limit_factor: miter_limit_factor.unwrap(),
+                },
+                _ => return Err(Error::from(InvalidData)),
+            },
+            no_h_scale,
+            no_v_scale,
+            pixel_hinting,
+            no_close,
+            end_cap_style,
+            fill_style,
+        })
+    }
 }
