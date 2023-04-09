a84f5bd397ee21c7bb9af70ba85d6b9cb9854c38
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 12:02:17 +0100

    DefineFontInfoTag::read: Accept any SizedRead as argument.

--- src/decode/tags/fonts/define_font_info.rs
+++ src/decode/tags/fonts/define_font_info.rs
@@ -1,5 +1,5 @@
 use crate::decode::read_ext::SwfTypesReadExt;
-use crate::decode::slice_reader::SwfSliceReader;
+use crate::decode::sized_read::SizedRead;
 use crate::decode::tags::common::string::String;
 use crate::decode::tags::fonts::code_table::CodeTable;
 use crate::decode::tags::fonts::define_font_info_flags::DefineFontInfoFlags;
@@ -14,7 +14,7 @@ pub struct DefineFontInfoTag {
 }
 
 impl DefineFontInfoTag {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: SizedRead>(reader: &mut R) -> Result<Self> {
         let font_id = reader.read_u16()?;
         let name_len = reader.read_u8()? as usize;
         let font_name = String::read_fixed_length(reader, name_len)?;
