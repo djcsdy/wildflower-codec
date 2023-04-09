b1b1ae91711a315312b86638f408ddc1e0f5c7a7
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 10:16:45 +0100

    DoActionTag::read: Accept any Read as argument.

--- src/decode/tags/actions/do_action.rs
+++ src/decode/tags/actions/do_action.rs
@@ -1,6 +1,5 @@
-use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::actions::action_list::ActionList;
-use std::io::Result;
+use std::io::{Read, Result};
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DoActionTag {
@@ -8,7 +7,7 @@ pub struct DoActionTag {
 }
 
 impl DoActionTag {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: Read>(reader: &mut R) -> Result<Self> {
         let actions = ActionList::read_to_end(reader)?;
         Ok(Self { actions })
     }
