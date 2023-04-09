2ad7616681de1f97fb12a1a8aa7c289adab9bf10
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 10:22:46 +0100

    ButtonRecordFlags::read: Accept any Read as argument.

--- src/decode/tags/buttons/button_record_flags.rs
+++ src/decode/tags/buttons/button_record_flags.rs
@@ -1,6 +1,5 @@
 use crate::decode::read_ext::SwfTypesReadExt;
-use crate::decode::slice_reader::SwfSliceReader;
-use std::io::Result;
+use std::io::{Read, Result};
 
 bitflags! {
     pub struct ButtonRecordFlags: u8 {
@@ -15,7 +14,7 @@ bitflags! {
 }
 
 impl ButtonRecordFlags {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: Read>(reader: &mut R) -> Result<Self> {
         Ok(Self::from_bits(reader.read_u8()?).unwrap())
     }
 }
