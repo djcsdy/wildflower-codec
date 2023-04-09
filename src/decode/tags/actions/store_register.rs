35290ab00b7fe89c985973989c6abeb1d5cd066a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 09:44:47 +0100

    StoreRegister::read: Accept any Read as argument.

--- src/decode/tags/actions/store_register.rs
+++ src/decode/tags/actions/store_register.rs
@@ -1,6 +1,5 @@
 use crate::decode::read_ext::SwfTypesReadExt;
-use crate::decode::slice_reader::SwfSliceReader;
-use std::io::Result;
+use std::io::{Read, Result};
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct StoreRegister {
@@ -8,7 +7,7 @@ pub struct StoreRegister {
 }
 
 impl StoreRegister {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: Read>(reader: &mut R) -> Result<Self> {
         let register_number = reader.read_u8()?;
         Ok(Self { register_number })
     }
