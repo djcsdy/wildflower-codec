069a50ea22dd2106ae249b23327581b6d778bbb2
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 10:06:40 +0100

    With::read: Accept any Read as argument.

--- src/decode/tags/actions/with.rs
+++ src/decode/tags/actions/with.rs
@@ -1,7 +1,6 @@
 use crate::decode::read_ext::SwfTypesReadExt;
-use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::actions::action_list::ActionList;
-use std::io::Result;
+use std::io::{Read, Result};
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct With {
@@ -9,7 +8,7 @@ pub struct With {
 }
 
 impl With {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: Read>(reader: &mut R) -> Result<Self> {
         let size = reader.read_u16()?;
         let body = ActionList::read(reader, size as usize)?;
         Ok(Self { body })
