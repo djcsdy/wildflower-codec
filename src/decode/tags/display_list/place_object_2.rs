cc8e8d07ca3165943ebb8560f7383d09d3b8badf
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 09:24:32 +0100

    Move read_place_object_2_tag function to PlaceObject2Tag::read.

--- src/decode/tags/display_list/place_object_2.rs
+++ src/decode/tags/display_list/place_object_2.rs
@@ -1,7 +1,12 @@
+use crate::decode::bit_read::BitRead;
+use crate::decode::read_ext::SwfTypesReadExt;
+use crate::decode::read_options::ReadOptions;
+use crate::decode::slice_read::SliceRead;
 use crate::decode::tags::common::color_transform_with_alpha::ColorTransformWithAlpha;
 use crate::decode::tags::common::matrix::Matrix;
 use crate::decode::tags::common::string::String;
 use crate::decode::tags::display_list::clip_actions::ClipActions;
+use std::io::Result;
 
 /// Adds a character to the display list.
 ///
@@ -52,4 +57,72 @@ pub struct PlaceObject2Tag {
     ///
     /// Valid only when placing or modifying sprite characters.
     pub clip_actions: Option<ClipActions>,
+}
+
+impl PlaceObject2Tag {
+    pub fn read<R: BitRead + SliceRead>(
+        ReadOptions {
+            reader,
+            swf_version,
+        }: ReadOptions<R>,
+    ) -> Result<Self> {
+        let has_clip_actions = reader.read_bit()?;
+        let has_clip_depth = reader.read_bit()?;
+        let has_name = reader.read_bit()?;
+        let has_ratio = reader.read_bit()?;
+        let has_color_transform = reader.read_bit()?;
+        let has_matrix = reader.read_bit()?;
+        let has_character = reader.read_bit()?;
+        let modify = reader.read_bit()?;
+        let depth = reader.read_u16()?;
+        let character_id = if has_character {
+            Some(reader.read_u16()?)
+        } else {
+            None
+        };
+        let matrix = if has_matrix {
+            Some(Matrix::read(reader)?)
+        } else {
+            None
+        };
+        let color_transform = if has_color_transform {
+            Some(ColorTransformWithAlpha::read(reader)?)
+        } else {
+            None
+        };
+        let ratio = if has_ratio {
+            Some(reader.read_u16()?)
+        } else {
+            None
+        };
+        let name = if has_name {
+            Some(String::read(reader)?)
+        } else {
+            None
+        };
+        let clip_depth = if has_clip_depth {
+            Some(reader.read_u16()?)
+        } else {
+            None
+        };
+        let clip_actions = if has_clip_actions {
+            Some(ClipActions::read(ReadOptions {
+                reader,
+                swf_version,
+            })?)
+        } else {
+            None
+        };
+        Ok(Self {
+            modify,
+            depth,
+            character_id,
+            matrix,
+            color_transform,
+            ratio,
+            name,
+            clip_depth,
+            clip_actions,
+        })
+    }
 }
--- src/extract/encoded_tag.rs
+++ src/extract/encoded_tag.rs
@@ -1,7 +1,7 @@
 use crate::decode::read_options::ReadOptions;
 use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::swf_version::SwfVersion;
-use crate::decode::tag_readers::display_list::{read_place_object_2_tag, read_place_object_3_tag};
+use crate::decode::tag_readers::display_list::read_place_object_3_tag;
 use crate::decode::tag_readers::shape_morphing::{
     read_define_morph_shape_2_tag, read_define_morph_shape_tag,
 };
@@ -33,6 +33,7 @@ use crate::decode::tags::control::set_background_color::SetBackgroundColorTag;
 use crate::decode::tags::control::set_tab_index::SetTabIndexTag;
 use crate::decode::tags::control::symbol_class::SymbolClassTag;
 use crate::decode::tags::display_list::place_object::PlaceObjectTag;
+use crate::decode::tags::display_list::place_object_2::PlaceObject2Tag;
 use crate::decode::tags::display_list::remove_object::RemoveObjectTag;
 use crate::decode::tags::display_list::remove_object_2::RemoveObject2Tag;
 use crate::decode::tags::fonts::define_font::DefineFontTag;
@@ -144,12 +145,12 @@ impl EncodedTag {
                     .unwrap_or_else(|_| Tag::Invalid(self.into_invalid()))
             }
             TagType::Protect => Tag::Unknown(self.into_unknown()),
-            TagType::PlaceObject2 => read_place_object_2_tag(ReadOptions {
+            TagType::PlaceObject2 => PlaceObject2Tag::read(ReadOptions {
                 reader: &mut slice_reader,
                 swf_version: self.swf_version,
             })
-            .map(Tag::PlaceObject2)
-            .unwrap_or_else(|_| Tag::Invalid(self.into_invalid())),
+                .map(Tag::PlaceObject2)
+                .unwrap_or_else(|_| Tag::Invalid(self.into_invalid())),
             TagType::RemoveObject2 => RemoveObject2Tag::read(&mut slice_reader)
                 .map(Tag::RemoveObject2)
                 .unwrap_or_else(|_| Tag::Invalid(self.into_invalid())),
