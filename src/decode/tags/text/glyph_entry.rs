9014ddc6d58a374cbf839e0524d107665fc91d0a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 12:28:39 +0100

    GlyphEntry::read: Accept any BitRead as argument.

--- src/decode/tags/text/glyph_entry.rs
+++ src/decode/tags/text/glyph_entry.rs
@@ -1,5 +1,4 @@
 use crate::decode::bit_read::BitRead;
-use crate::decode::slice_reader::SwfSliceReader;
 use std::io::Result;
 
 #[derive(Clone, PartialEq, Debug)]
@@ -8,19 +7,19 @@ pub struct GlyphEntry {
     pub glyph_advance: i32,
 }
 
-pub struct ReadGlyphEntryOptions<'reader, 'buffer> {
-    pub reader: &'reader mut SwfSliceReader<'buffer>,
+pub struct ReadGlyphEntryOptions<'reader, Read: BitRead> {
+    pub reader: &'reader mut Read,
     pub glyph_bits: u8,
     pub advance_bits: u8,
 }
 
 impl GlyphEntry {
-    pub fn read(
+    pub fn read<R: BitRead>(
         ReadGlyphEntryOptions {
             reader,
             glyph_bits,
             advance_bits,
-        }: ReadGlyphEntryOptions,
+        }: ReadGlyphEntryOptions<R>,
     ) -> Result<Self> {
         let glyph_index = reader.read_ub(glyph_bits)?;
         let glyph_advance = reader.read_sb(advance_bits)?;
