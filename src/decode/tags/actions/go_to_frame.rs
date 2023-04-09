25a77c31118cad17f5d9021a7d45a3f41c56bc74
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 09:43:34 +0100

    GoToFrame::read: Accept any Read as argument.

--- src/decode/tags/actions/go_to_frame.rs
+++ src/decode/tags/actions/go_to_frame.rs
@@ -1,6 +1,5 @@
 use crate::decode::read_ext::SwfTypesReadExt;
-use crate::decode::slice_reader::SwfSliceReader;
-use std::io::Result;
+use std::io::{Read, Result};
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct GoToFrame {
@@ -8,7 +7,7 @@ pub struct GoToFrame {
 }
 
 impl GoToFrame {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: Read>(reader: &mut R) -> Result<Self> {
         let frame = reader.read_u16()?;
         Ok(Self { frame })
     }
