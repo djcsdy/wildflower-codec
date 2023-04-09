cbb0dbbfb216fdba9353416dca3f11c3807fc9f2
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 11:02:30 +0100

    AlignZoneRecord::read: Accept any Read as argument.

--- src/decode/tags/fonts/align_zone_record.rs
+++ src/decode/tags/fonts/align_zone_record.rs
@@ -1,8 +1,7 @@
 use crate::decode::read_ext::SwfTypesReadExt;
-use crate::decode::slice_reader::SwfSliceReader;
 use crate::decode::tags::fonts::align_zone_data::AlignZoneData;
 use crate::decode::tags::fonts::align_zone_flags::AlignZoneFlags;
-use std::io::Result;
+use std::io::{Read, Result};
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct AlignZoneRecord {
@@ -11,7 +10,7 @@ pub struct AlignZoneRecord {
 }
 
 impl AlignZoneRecord {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: Read>(reader: &mut R) -> Result<Self> {
         let num_zone_data = reader.read_u8()?;
         let mut zone_data = Vec::with_capacity(num_zone_data as usize);
         for _ in 0..num_zone_data {
