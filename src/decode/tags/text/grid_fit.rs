916148c091aad08bed8d371e5470479ac3a15715
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 12:18:19 +0100

    GridFit::read: Accept any BitRead as argument.

--- src/decode/tags/text/grid_fit.rs
+++ src/decode/tags/text/grid_fit.rs
@@ -1,5 +1,4 @@
 use crate::decode::bit_read::BitRead;
-use crate::decode::slice_reader::SwfSliceReader;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 use std::io::ErrorKind::InvalidData;
 use std::io::{Error, Result};
@@ -13,7 +12,7 @@ pub enum GridFit {
 }
 
 impl GridFit {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: BitRead>(reader: &mut R) -> Result<Self> {
         Self::try_from(reader.read_ub8(3)?).map_err(|_| Error::from(InvalidData))
     }
 }
