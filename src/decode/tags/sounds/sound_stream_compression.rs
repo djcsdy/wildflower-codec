45888b600fc6ce23524bdf53423795dfe2e7a48a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 12:11:52 +0100

    SoundStreamCompression::read: Accept any BitRead as argument.

--- src/decode/tags/sounds/sound_stream_compression.rs
+++ src/decode/tags/sounds/sound_stream_compression.rs
@@ -1,5 +1,4 @@
 use crate::decode::bit_read::BitRead;
-use crate::decode::slice_reader::SwfSliceReader;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 use std::io::ErrorKind::InvalidData;
 use std::io::{Error, Result};
@@ -12,7 +11,7 @@ pub enum SoundStreamCompression {
 }
 
 impl SoundStreamCompression {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: BitRead>(reader: &mut R) -> Result<Self> {
         Self::try_from(reader.read_ub8(2)?).map_err(|_| Error::from(InvalidData))
     }
 }
