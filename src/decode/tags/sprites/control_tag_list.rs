d2bdc5e178cd92acfb26d961f34cf1b5f58da517
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 12:14:57 +0100

    ControlTagList::read_to_end: Accept any Read as argument.

--- src/decode/tags/sprites/control_tag_list.rs
+++ src/decode/tags/sprites/control_tag_list.rs
@@ -1,6 +1,5 @@
 use crate::decode::read_ext::SwfTypesReadExt;
-use crate::decode::slice_reader::SwfSliceReader;
-use std::io::Result;
+use std::io::{Read, Result};
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct ControlTagList<Buffer: AsRef<[u8]>> {
@@ -8,7 +7,7 @@ pub struct ControlTagList<Buffer: AsRef<[u8]>> {
 }
 
 impl ControlTagList<Vec<u8>> {
-    pub fn read_to_end(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read_to_end<R: Read>(reader: &mut R) -> Result<Self> {
         let buffer = reader.read_u8_to_end()?;
         Ok(Self { buffer })
     }
