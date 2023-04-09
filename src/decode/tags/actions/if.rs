92805c78f56b4e42140b5d49bed5bbd570913a22
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-04-09 10:13:04 +0100

    If::read: Accept any Read as argument.

--- src/decode/tags/actions/if.rs
+++ src/decode/tags/actions/if.rs
@@ -1,6 +1,5 @@
 use crate::decode::read_ext::SwfTypesReadExt;
-use crate::decode::slice_reader::SwfSliceReader;
-use std::io::Result;
+use std::io::{Read, Result};
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct If {
@@ -8,7 +7,7 @@ pub struct If {
 }
 
 impl If {
-    pub fn read(reader: &mut SwfSliceReader) -> Result<Self> {
+    pub fn read<R: Read>(reader: &mut R) -> Result<Self> {
         let offset = reader.read_i16()?;
         Ok(Self { offset })
     }
